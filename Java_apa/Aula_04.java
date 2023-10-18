ba885b20457978924ee28341f32c940d8dfa14e7
Guilherme <Fireguiquenbr@gmail.com>
2023-10-18 20:53:19 -0300

    Update Aula_04.java

--- Java_apa/Aula_04.java
+++ Java_apa/Aula_04.java
@@ -5,9 +5,11 @@ public class Aula_04{
     public static void main(String[] args){
         
         // Fizemos apenas uma declaração. Como é do tipo "int", a variável aceita somente números INTEIROS!! 
+        // sintaxe: tipo de dado - nomeDaVariavel 
         int x; 
 
-        // Agora, podemos atruibuir um valor a ela
+        // Agora, podemos atruibuir um valor a ela;
+        // sintaxe: nomeDaVariavel - sinalDeAtribuição(=) - valor que desejamos (precisa ser um número inteiro)
         x = 50;
 
         // Anteriomente usamos aspas duplas pois queriamos printar STRINGS..
