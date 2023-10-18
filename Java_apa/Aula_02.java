cc02511c2c2b8d3e0618c77c39836aa0bbf93d6a
Guilherme <Fireguiquenbr@gmail.com>
2023-10-18 19:40:28 -0300

    Update Aula_02.java

--- Java_apa/Aula_02.java
+++ Java_apa/Aula_02.java
@@ -2,7 +2,10 @@ package java_apa; // package -> informa qual o pacote da nossa classe.
 
 // os colchetes representam um bloco. 
 // tudo que é colocado dentro deles pertencem a nossa classe. 
-public class Aula_02{
+public class Aula_02
+{
 
-    public static void main(String[] args)
+    public static void main(String[] args){
+
+    }
 }
