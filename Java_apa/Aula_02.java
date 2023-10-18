5b682d98b1cbdc39e8b6d1993e15a0bb74faae0d
Guilherme <Fireguiquenbr@gmail.com>
2023-10-18 19:43:02 -0300

    Update Aula_02.java

--- Java_apa/Aula_02.java
+++ Java_apa/Aula_02.java
@@ -5,9 +5,10 @@ package java_apa; // package -> informa qual o pacote da nossa classe.
 public class Aula_02
 {
 
-    // esse é o nosso método
-    // informa o ponto de partida do nosso programa;
-    // é como se fosse a função "main" da linguagem C.
+    /* esse é o nosso método e ele informa o ponto de partida do nosso programa;
+    para informar ao Java que é o nosso método de inicio, usamos a palavra-chave "main" (principal)
+    Logo ele sabe que o programa deve executar com este método. 
+    -> é como se fosse a função "main" da linguagem C.                                              */
     public static void main(String[] args){
 
     }
