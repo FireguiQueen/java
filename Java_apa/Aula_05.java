7e5b48b487145eb1e1bc3949aeabde222bb092e7
Guilherme <Fireguiquenbr@gmail.com>
2023-10-18 20:57:52 -0300

    Create Aula_05.java

---
+++ Java_apa/Aula_05.java
@@ -0,0 +1,17 @@
+// VARIÁVEIS II 
+
+class Aula_05
+{
+
+    public static void main(String[] args){
+
+        // Podemos atribuir a uma variável outras variáveis. 
+        // Como neste caso, onde estamos dizendo "a variávei total RECEBE a variável x mais variável y"
+
+        int x = 50;
+        int y = 50;
+
+        int total = y + x; 
+        // resposta (output): 100;  
+    }
+}
