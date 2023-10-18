e1568be997396b6d67b3d0ee10fde0cf03f4a388
Guilherme <Fireguiquenbr@gmail.com>
2023-10-18 19:41:24 -0300

    Update Aula_02.java

--- Java_apa/Aula_02.java
+++ Java_apa/Aula_02.java
@@ -5,6 +5,9 @@ package java_apa; // package -> informa qual o pacote da nossa classe.
 public class Aula_02
 {
 
+    // esse é o nosso método
+    // informa o ponto de partida do nosso programa;
+    // é como se fosse a função "main" da linguagem C.
     public static void main(String[] args){
 
     }
