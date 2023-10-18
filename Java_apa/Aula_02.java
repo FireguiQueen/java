3df60d110c51d1d270aaae827e89adca5668d50b
Guilherme <Fireguiquenbr@gmail.com>
2023-10-18 19:47:05 -0300

    update

--- Java_apa/Aula_02.java
+++ Java_apa/Aula_02.java
@@ -1,4 +1,4 @@
-package java_apa; // package -> informa qual o pacote da nossa classe. 
+package Java_apa; // package -> informa qual o pacote da nossa classe. 
 
 // os colchetes representam um bloco. 
 // tudo que é colocado dentro deles pertencem a nossa classe. 
@@ -10,6 +10,7 @@ public class Aula_02
     Logo ele sabe que o programa deve executar com este método. 
     -> é como se fosse a função "main" da linguagem C.                                              */
     public static void main(String[] args){
+        System.out.println("Olá!");
 
     }
 }
