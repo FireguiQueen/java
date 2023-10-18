93abc8aa7b717ef09fac64ebf29ad51f94dd0c81
Guilherme <Fireguiquenbr@gmail.com>
2023-10-18 20:44:03 -0300

    Update Aula_04.java

--- Java_apa/Aula_04.java
+++ Java_apa/Aula_04.java
@@ -8,12 +8,25 @@ public class Aula_04{
         int x; 
 
         // Agora, podemos atruibuir um valor a ela
-        x = 500;
+        x = 50;
 
         // Anteriomente usamos aspas duplas pois queriamos printar STRINGS..
         // Mas agora, desejamos printar valores de variáveis, sendo assim, precisamos colocar fora das aspas.
         // Pois as aspas servem exclusivamente para strings.
         // Por exemplo, se utilizassemos aspas na linha abaixo, o CARACTER 'x' que seria printado, e não a variável.
         System.out.println(x);
+
+
+        // O valor que colocamos na linha 17 é uma variável, sendo assim, estamos apenas nos referindo ao valor dela.
+        /* Porém, em nosso terminal, o será retornado mesmo é uma string. Então mesmo que você coloque um valor inteiro,
+        um valor quebrado ou qualquer outro, o que será retornado no terminal será STRING! */
+
+        // Para concatenar os valores em Java (juntar valores) podemos usar o caracter '+'.
+        // Então por exemplo, para juntar o valor da nossa variável + uma string é algo bem simples:
+        System.out.println("José tem " + x " anos");
+        // resultado (output): "José tem 50 anos"
+        
+        /* observe que no resultado o valor de x está entre aspas também, ou seja, podemos ver que de fato  
+        tudo colocado ali terá um retorno sendo string. */
     }
 }
