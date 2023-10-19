ecf2f877d470c603e05d4c3746867eeef4d48ed4
Guilherme <Fireguiquenbr@gmail.com>
2023-10-19 12:29:53 -0300

    update

---
+++ Java_apa/Aula_10.java
@@ -0,0 +1,19 @@
+// IMPORTAÇÃO de pacotes e classes
+
+// Import é "importar", ou seja, estamos dizendo que queremos importar (trazer) algo para o nosso programa.
+// Nesse caso, estamos importando a classe "Scanner" do pacote "util".
+// Sem esta importação, não seria possível utilizar essa classe em nosso programa.
+import java.util.Scanner;  
+
+
+// Como podemos importar TODAS as classes de uma vez só de um pacote?
+// É bem simples, basta espeificar o pacote e invés de escolhar uma classe, você coloca o asterisco (*).
+import java.util.*;
+
+public class Aula_10{
+
+    public static void main(String[] args)
+    {
+        Scanner sr = new Scanner(System.in);
+    }
+}
--- Java_apa/Java_10.java
+++
@@ -1,4 +0,0 @@
-
-
-
-import java.util.Scanner; 
