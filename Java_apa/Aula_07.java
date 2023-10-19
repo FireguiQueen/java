e5a875228cd26f20ec53a53de176e5037b6db9cd
Guilherme <Fireguiquenbr@gmail.com>
2023-10-18 23:33:38 -0300

    Create Aula_07.java

---
+++ Java_apa/Aula_07.java
@@ -0,0 +1,26 @@
+// Tipos de dados - primitivos 
+
+class Aula_06{
+
+    public static void main(String[] args){
+
+        // Tipos para números INTEIROS
+        byte num;
+        short num1;
+        int num2; 
+        long num3; 
+
+
+        // Tipos para números QUEBRADOS
+        float num4; 
+        double num5; 
+
+
+        // Tipo para valores de VERDADEIRO ou FALSO (0 ou 1)
+        boolean isRaining = true; 
+
+        // Tipo para CARACTERES
+        char firstLetterAlphabet = 'a'
+
+    }
+}
