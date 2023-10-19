d8fb7a874a4f10c9ee3995f6f0e0de3c97d58d03
Guilherme <Fireguiquenbr@gmail.com>
2023-10-19 19:43:17 -0300

    update

--- Java_apa/Aula_06.java
+++ Java_apa/Aula_06.java
@@ -1,17 +1,18 @@
-// Tipos de dados 
+// TIPOS DE DADOS
 
 class Aula_06{
 
     public static void main(String[] args){
 
-        // Uma variável que recebe um número inteiro é utilizado o 'int' para ela:
+        // Variáveis que recebem números inteiros usam o tipo 'int'. E esse tipo aceita apenas números inteiros.
         int y = 10; 
 
-        // Para fazer um número quebrado precisamos definir que o seu tipo é double.
+        // Para atribuir um número quebrado precisamos usar o tipo "double" ou "float".
         double x = 10.5;
 
 
-        // O double não é importante apenas para isso, pois nem sempre iremos trabalhar com números quebrados..
+        // Os tipos double e float não são importantes apenas para isso, pois nem sempre iremos trabalhar com 
+        // números quebrados..
         // Mas não trabalhar com eles diretamente, não significa que não geraremos eles.
 
         // Por exemplo:
@@ -20,14 +21,16 @@ class Aula_06{
         int numero_02 = 4; 
 
         // Mas e se realizarmos uma divisão entre eles? 
-        System.out.println(numero_01 / numero_02); // 150 dividido por 4 = 37.5 
+        // 150 dividido por 4 = 37.5 (retorna um número quebrado)
+        
+        System.out.println(numero_01 / numero_02);
         // output: 37 
 
         // O nosso output foi 37 e não 37.5, ou seja, o nosso número quebrado foi totalmente ignorado. 
 
-        // E caso não houvesse um número inteiro 
+        // E caso não houvesse um número inteiro, como neste exemplo:  
         System.out.println(4 / 150); // 4 dividido por 150 = 0,02 (ou seja, não há números inteiros)
-        // o nosso output será 0.
+        // o nosso output será 0. Então vemos que de fato, apenas números inteiros estão sendo retornados.
 
 
         // Você pode pensar: "bem, a solução é bem simples :o, basta colocar o tipo de dado 'double' xD xD"
---
+++ Java_apa/Aula_12.java
@@ -0,0 +1,10 @@
+public class Aula_12{
+
+    public static void main(String[] args)
+    {
+        int x = 10;
+        float y = 10.5;
+
+        // System.out.print(10 + 10.5);
+    }
+}
