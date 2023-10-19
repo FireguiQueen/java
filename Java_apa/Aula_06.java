eb75b2c32d540face53570265bd502cb521a09e5
Guilherme <Fireguiquenbr@gmail.com>
2023-10-19 19:55:23 -0300

    Update Aula_06.java

--- Java_apa/Aula_06.java
+++ Java_apa/Aula_06.java
@@ -4,23 +4,20 @@ class Aula_06{
 
     public static void main(String[] args){
 
-        // Variáveis que recebem números inteiros usam o tipo 'int'. E esse tipo aceita apenas números inteiros.
+        // variáveis que armazenam números inteiros usam o tipo 'int'. Esse tipo aceita apenas valores inteiros.
         int y = 10; 
 
-        // Para atribuir um número quebrado podemos usar o "double", mas também existe o "float".
+        // Para armazenar números de ponto flutuante, como números quebrados, usamos 'double' ou 'float'.
         double x = 10.5;
 
 
-        // Os tipos double e float não são importantes apenas para isso, pois nem sempre iremos trabalhar com 
-        // números quebrados.
-        // Mas não trabalhar com eles diretamente, não significa que não geraremos eles.
 
+        // Não usá-los diretamente, não significa que não geraremos eles através de cálculos no código.
         // Por exemplo:
-        // Observe que daqui pra baixo há somente números inteiros. 
         int numero_01 = 150;
         int numero_02 = 4; 
 
-        // Mas e se realizarmos uma divisão entre eles? 150 dividido por 4 = 37.5 (retorna um número quebrado)
+        // Agora vamos realizar a divisão. 150 dividido por 4 = 37.5 (retorna um número quebrado)
         System.out.println(numero_01 / numero_02);
         // output: 37 
         // O nosso output foi 37 e não 37.5, ou seja, o nosso número quebrado foi totalmente ignorado. 
@@ -34,12 +31,13 @@ class Aula_06{
         // Você pode pensar: "bem, a solução é bem simples :o, basta colocar o tipo de dado 'double' xD xD"
         int num0 = 150;
         int num1 = 4;
-
         double divisaoFinal = num0 / num1; // 150 dividido por 4. Resultado esperado: 37.5  
+        
         System.out.println(divisaoFinal);
         // ouput: 37 
 
-        // Mas não, não é assim que o Java funciona. Como podemos ver, o output foi de 37, e não 37.5, mas por que?
+        // Mas não, não é assim que o Java funciona. Como podemos ver, o output foi de 37, e não 37.5. 
+        // Mas por que?
         /*
             Um número inteiro dividido por outro número inteiro RESULTARÁ sempre em um número inteiro.
             INDEPENDENTE do tipo de dado que irá armazenar os valores. 
@@ -47,7 +45,7 @@ class Aula_06{
             Quando dizemos que uma variável do tipo 'double' irá armazenar a divisão de 150 por 4, estamos
             apenas dizendo ao Java que aquela variável irá SURPORTAR um valor quebrado. 
             Mas como o valor daquela variável foi um inteiro dividido por outro, o resultado é um inteiro.  
-
+        */
 
     }
 }
