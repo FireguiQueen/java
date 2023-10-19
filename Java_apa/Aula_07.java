ded7c3c3f5c80ed1fd6cb82212d2dfc62aaf30e9
Guilherme <Fireguiquenbr@gmail.com>
2023-10-19 10:41:03 -0300

    Update Aula_07.java

--- Java_apa/Aula_07.java
+++ Java_apa/Aula_07.java
@@ -5,19 +5,19 @@ class Aula_06{
     public static void main(String[] args){
 
         // Tipos para números INTEIROS
-        byte num;
-        short num1;
-        int num2; 
-        long num3; 
+        byte num;   // para valores muitos baixos  -- utiliza apenas UM byte para formar números;
+        short num1; // para valores baixos         -- utiliza DOIS bytes para formar números;
+        int num2;   // para valores médios         -- utiliza QUATRO bytes para formar números;
+        long num3;  // para valores altos          -- utiliza OITO bytes para formar números.
 
 
         // Tipos para números QUEBRADOS
-        float num4; 
-        double num5; 
+        float num4;  // os números após a vírgula terão uma precisão menor
+        double num5; // os números após a vírgula terão uma precisão maior
 
 
         // Tipo para valores de VERDADEIRO ou FALSO (0 ou 1)
-        boolean isRaining = true; 
+        boolean isRaining = true; // "está chovendo? TRUE (verdadeiro)". É muito utilizado em condições 
 
         // Tipo para CARACTERES
         char firstLetterAlphabet = 'a'
