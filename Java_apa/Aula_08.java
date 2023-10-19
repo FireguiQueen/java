171023ec870d97aa2c9bbcc9041beaacc7c60acc
Guilherme <Fireguiquenbr@gmail.com>
2023-10-19 11:31:12 -0300

    Create Aula_08.java

---
+++ Java_apa/Aula_08.java
@@ -0,0 +1,19 @@
+// PALAVRAS-CHAVES
+
+public class Aula_08{
+
+    public static void main(String[] args){
+
+        // Como qualquer outral linguagem de programação, o Java também precisa de palavras para poder definir
+        // como as coisas vão funcionar na linguagem. 
+        /*
+            Por exemplo, em java precisamos definir o tipo de dado de nossa variável, sendo assim, o java precisa
+            de algumas palavras-chaves para isso, entre elas temos o int, float, double.. 
+        */
+
+        // Observe que NÃO podemos usar as palavras-chave do java para nós como bem entendermos.
+        // Como no exemplo abaixo, onde tentamos definir que o nome da variável é 'false', mas o 'false' já é
+        // uma palavra-chave do java. 
+        int false = 5; 
+    }
+}
