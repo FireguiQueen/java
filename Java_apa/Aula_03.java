dcb1457c4452205a1a84e7200f9941863789714b
Guilherme <Fireguiquenbr@gmail.com>
2023-10-19 19:26:08 -0300

    Update Aula_03.java

--- Java_apa/Aula_03.java
+++ Java_apa/Aula_03.java
@@ -1,16 +1,13 @@
 // COMENTÁRIOS
 
-
 class Aula_02{
 
     /*
-        Caso não utilizemos as barras duplas (comentário de linha) (//)
-
-        Ou as barras com asteriscos (comentários de bloco) para colocar algo que não faça parte da linguagem,
-        como por exemplo uma expliclação, o java não vai entender como compilar esse determinado trecho
+        Caso não utilizemos as barras duplas (comentário de linha) (//) ou as barras com asteriscos (comentários 
+        de bloco) para colocar algo que não faça parte da linguagem (como por exemplo uma expliclação), o 
+        java não vai entender como compilar esse determinado trecho, e retornará erro.
     */
 
-    
     public static void main(String[] args){
         System.out.println("Olá!");
 
