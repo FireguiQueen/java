142020e793b525194340b6261c81849c3098430d
Guilherme <Fireguiquenbr@gmail.com>
2023-10-20 00:20:20 -0300

    update

---
+++ Java_apa/exercicios/soma_I.java
@@ -0,0 +1,13 @@
+public class soma_I
+{
+    public static void main(String[] args)
+    {
+        double x, y, total; 
+
+        x = 24;
+        y = 65.4;
+        total = x + y;
+
+        System.out.println("Resultado: " + total);
+    }
+}
---
+++ Java_apa/exercicios/soma_II.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class soma_II
+{
+    public static void main(String[] args)
+    {
+        float x, y, total;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o valor de X: ");
+        x = input.nextFloat();
+
+        System.out.print("Digite o valor de Y: ");
+        y = input.nextFloat();
+
+        total = x + y;
+
+        System.out.println("Resultado: " + total);
+    }
+}
