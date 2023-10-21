8e6d536113588e81a9fd6c15f06e01185203ee45
Guilherme <Fireguiquenbr@gmail.com>
2023-10-20 21:26:58 -0300

    Update Aula_13.java

--- Java_deAlunoParaAluno/Aula_13.java
+++ Java_deAlunoParaAluno/Aula_13.java
@@ -3,12 +3,15 @@
     Em java, podemos transformar nossas variáveis em constantes (ou seja, o valor NÃO pode ser alterado)
     utilizando a palavra-chave 'final' antes do tipo de dado.
 
-    Utilizaremos constantes quando vejos que o valor sempre será fixo, e que não precisa sofrer alterações.  
+    Utilizaremos constantes quando vejos que o valor sempre será fixo, e que não precisa sofrer alterações.
+    É uma boa prática os nomes das variáveis constantes serem em letras maiúsculas.   
 */ 
 
 public class Aula_13{
     public static void main(String[] args)
     {
-        final int cpf = 04133624780;
+        final int CPF = 04133624780;
+
+        System.out.println("CPF: " + CPF);
     }
 }
