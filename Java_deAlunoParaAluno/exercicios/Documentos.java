155156a36de5f0ca9c6a3c315871c993ce000e92
Guilherme <Fireguiquenbr@gmail.com>
2023-10-20 21:46:18 -0300

    Update Documentos.java

--- Java_deAlunoParaAluno/exercicios/Documentos.java
+++ Java_deAlunoParaAluno/exercicios/Documentos.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class Documentos{
-    
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Digite seu CPF: ");
+        final long CPF = input.nextLong();
+
+        System.out.print("Sua idade: ");
+        final int IDADE = input.nextInt();
+
+        System.out.println(CPF + ", " + IDADE);
+
+    }
 }
