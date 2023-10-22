28383b71906331041975fa1468395da69805ac46
Guilherme <Fireguiquenbr@gmail.com>
2023-10-22 16:27:19 -0300

    Update Aula_07.java

--- Java_deAlunoParaAluno/Aula_07.java
+++ Java_deAlunoParaAluno/Aula_07.java
@@ -6,15 +6,15 @@ class Aula_06{
     public static void main(String[] args){
 
         // Tipos para números INTEIROS
-        byte num;   // para valores muitos baixos  -- utiliza apenas UM byte para formar números;
-        short num1; // para valores baixos         -- utiliza DOIS bytes para formar números;
-        int num2;   // para valores médios         -- utiliza QUATRO bytes para formar números;
-        long num3;  // para valores altos          -- utiliza OITO bytes para formar números.
+        byte num;       // para valores muitos baixos  -- utiliza apenas UM byte para formar números;
+        short num1;     // para valores baixos         -- utiliza DOIS bytes para formar números;
+        int num2;       // para valores médios         -- utiliza QUATRO bytes para formar números;
+        long num3;      // para valores altos          -- utiliza OITO bytes para formar números.
 
 
         // Tipos para números QUEBRADOS
-        float num4;  // os números após a vírgula terão uma precisão menor
-        double num5; // os números após a vírgula terão uma precisão maior
+        float num4;     // os números após a vírgula terão uma precisão menor
+        double num5;    // os números após a vírgula terão uma precisão maior
 
 
         // Tipo para valores de VERDADEIRO ou FALSO (0 ou 1)
@@ -22,6 +22,5 @@ class Aula_06{
 
         // Tipo para CARACTERES
         char firstLetterAlphabet = 'a'
-
     }
 }
