8dfb5621f8ef230a001c56dcf4f7bfce5f2b561e
Guilherme <Fireguiquenbr@gmail.com>
2023-10-22 16:20:38 -0300

    Update Aula_03.java

--- Java_deAlunoParaAluno/Aula_03.java
+++ Java_deAlunoParaAluno/Aula_03.java
@@ -6,6 +6,8 @@ class Aula_02{
         Caso não utilizemos as barras duplas (comentário de linha) (//) ou as barras com asteriscos (comentários 
         de bloco) para colocar algo que não faça parte da linguagem (como por exemplo uma expliclação), o 
         java não vai entender como compilar esse determinado trecho, e retornará erro.
+
+        Por exemplo, o que você está lendo agora, como não faz parte da linguagem, deve estar num comentário.
     */
 
     public static void main(String[] args){
