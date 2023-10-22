594308831669d6c9129f4056a533a239e9dc3e0f
Guilherme <Fireguiquenbr@gmail.com>
2023-10-22 16:18:51 -0300

    Update Aula_02.java

--- Java_deAlunoParaAluno/Aula_02.java
+++ Java_deAlunoParaAluno/Aula_02.java
@@ -1,15 +1,16 @@
 // ESTRUTURA 
 
+/* Por enquanto, como estamos de um ponto bem inicial da linguagem java, iremos ignorar algumas palavras-chave.
+Tais como: public, static, void, String[]... */ 
 
 // Os colchetes representam um bloco. Tudo que é colocado dentro deles pertencem a nossa classe. 
 // A classe é uma estrutura responsável por armazenar atributos e métodos.
 class Aula_02
 {
 
-    // Esse é o nosso método e ele informa o ponto de partida do nosso programa;
-    // Usamos a palavra-chave "main" justamente por isso, pois é o nosso método principal. 
+    // Esse é o nosso método principal e ele informa o ponto de partida do nosso programa;
+    // Usamos a palavra-chave "main" justamente por isso, pois PRECISAMOS de um método principal. 
     public static void main(String[] args){
         System.out.println("Olá!");
-
     }
 }
