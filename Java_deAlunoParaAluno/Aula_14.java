f3f986449f89362d1e246e6ad199b7e229dbe2e0
Guilherme <Fireguiquenbr@gmail.com>
2023-10-23 19:26:35 -0300

    Update Aula_14.java

--- Java_deAlunoParaAluno/Aula_14.java
+++ Java_deAlunoParaAluno/Aula_14.java
@@ -11,3 +11,24 @@
     E qualquer número dividido por dois que tenha o resto da divisão DIFERENTE de zero, será ímpar.  
 */
 
+import java.util.Scanner; 
+
+public class Aula_14{
+    public static void main(String[] args)
+    {
+        Scanner inputUser = new Scanner(System.in);
+
+        System.out.print("Digite o dividendo: ");
+        int dividendo = inputUser.nextInt();
+
+        System.out.print("Digite o divisor: ");
+        int divisor = inputUser.nextInt();
+
+        // A divisão normal seria por exemplo "10 / 5", o que resulta em dois.
+        // Com o resto, será "10 % 5", o que singifica "Qual será o resto da divisão quando dividimos 10 por 5?"
+        int resto = dividendo % divisor;
+        System.out.println("Resto da divisão: " + resto);
+
+        
+    }
+}
