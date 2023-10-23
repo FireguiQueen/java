17911e9359c3bc4f4bee6307f2eaea528cb2b6e4
Guilherme <Fireguiquenbr@gmail.com>
2023-10-23 19:14:35 -0300

    Create Aula_14.java

---
+++ Java_deAlunoParaAluno/Aula_14.java
@@ -0,0 +1,13 @@
+// RESTO DA DIVISÃO: %
+
+/* 
+    Em Java, e em outras diversas outras linguagens, o operador de resto de divisão é representado pela porcentagem (%).
+
+    O resto da divisão é um número maior ou igual a zero e menor que o divisor. Ele é o que sobra na 
+    aplicação do algoritmo em uma divisão de números inteiros.
+
+    Qual a sua utilidade? Este operador nos permite por exemplo, saber se determinado número é ímpar ou par. 
+    Qualquer número dividido por dois que tenha o resto da divisão igual a zero, será par. 
+    E qualquer número dividido por dois que tenha o resto da divisão DIFERENTE de zero, será ímpar.  
+*/
+
