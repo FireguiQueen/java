c1e9d195506d0909b3224fd3b69ffaa020f2113c
Guilherme <Fireguiquenbr@gmail.com>
2023-10-23 22:59:46 -0300

    Create Aula_17.java

---
+++ Java_deAlunoParaAluno/Aula_17.java
@@ -0,0 +1,14 @@
+// STRINGS
+
+import java.util.Scanner; 
+
+public class Aula_17{
+    public static void main(String[] args)
+    {
+        // Define uma string. Não é um tipo de dado primitivo, é um objeto. 
+        String nome = "Osvaldo";
+
+        System.out.println(nome);
+
+    }
+}
