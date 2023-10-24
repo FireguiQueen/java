b10ff0804e3ea4a6e40d95306d89926f9ea11569
Guilherme <Fireguiquenbr@gmail.com>
2023-10-24 18:59:34 -0300

    Update Aula_18.java

--- Java_deAlunoParaAluno/Aula_18.java
+++ Java_deAlunoParaAluno/Aula_18.java
@@ -12,10 +12,27 @@ public class Aula_18{
         // Sendo assim, será printado "olá" no terminal.
         if (10 > 0)
         {
-            System.out.println("Olá!");
-
-            
+            System.out.println("Olá!");   
         }
+
+        // Também podemos usar variáveis.
+        int idade = 18; 
+        if (idade < 18){
+            System.out.println("Você é de menor.");
+        }
+        // No caso acima, nossa variável idade recebe 18. Então nossa condição booleana se retorna "18 < 18".
+        // E isso obviamente retorna "false", pois 18 não é menor do que 18.
+
+        // Por outro lado, podemos colocar o sinal de "igual".
+        // Neste caso, estamos perguntando: "18 é MENOR ou IGUAL a 18?"
+        // E isto retorna "TRUE", mesmo que 18 não MENOR do que 18, 18 é igual a 18.
+        if (idade <= 18){
+            System.out.println("Você tem menos de 18 anos ou tem 18 anos.");
+        }
+
+        // Funcionamento do 'else'. 
+        // O else é como "caso o IF seja falso, execute este bloco"
+        if 
 
     }
 }
