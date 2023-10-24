dc0dd28ae14e3dc9586c2cfbbe7f5f377c5687b1
Guilherme <Fireguiquenbr@gmail.com>
2023-10-24 18:43:38 -0300

    Create Aula_18.java

---
+++ Java_deAlunoParaAluno/Aula_18.java
@@ -0,0 +1,19 @@
+// CONDICIOANIS: IF & ELSE
+
+public class Aula_18{
+    public static void main(String[] args)
+    {
+        // 'if' significa 'se'. Se determinada condinção for verdadeira (true), excute o bloco. 
+        // o 'if' trabalha com expressões booleanas, então por exemplo: 1 > 2 (um maior do que dois? FALSO).
+        // ou seja, o retorno desta expressão é a palavra "FALSE". 
+        
+        // SE dez for maior do que zero, excute o bloco. Como a expressão é verdadeira, o bloco será executado.
+        // Sendo assim, será printado "olá" no terminal.
+        if (10 > 0)
+        {
+            System.out.println("Olá!");
+        }
+
+        // 
+    }
+}
