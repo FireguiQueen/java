e706ef7a22ca63fcbc1090b12e5ddaa549083ad8
Guilherme <Fireguiquenbr@gmail.com>
2023-10-24 19:28:49 -0300

    update

--- Java_deAlunoParaAluno/Aula_19.java
+++ Java_deAlunoParaAluno/Aula_19.java
@@ -1,1 +1,22 @@
 // CONDICIOANIS: IF & ELSE II 
+
+public class Aula_19{
+    public static void main(String[] args)
+    {
+        // Anteriormente, na aula 18, vimos e utilizamos apenas uma única condicional (IF). 
+        // Mas e se precisássemos de mais condições? 
+
+        /* 
+            Por exemplo, utilizando apenas "if" e 'else' somos capazes de colocar uma mensagem para 
+            maiores de 18 e para menores de 18 anos.                                        */ 
+        byte idade = 18;
+        if(idade >= 18){
+            System.out.println("Você é de maior.");
+        } else {
+            System.out.println("Você é de menor.");
+        }
+
+
+        // Mas como vamos colocar uma mensagem especifica para quem colocar uma idade negativa? 
+    }
+}
--- Java_deAlunoParaAluno/exercicios/Verificar_idade.java
+++ Java_deAlunoParaAluno/exercicios/Verificar_idade.java
@@ -5,14 +5,17 @@ public class Verificar_idade{
     {
         Scanner userInput = new Scanner(System.in);
 
-        System.out.println("Digite sua idade: ");
+        System.out.print("Digite seu nome: ");
+        final String NOME = userInput.nextLine();
+
+        System.out.print("Digite sua idade: ");
         final byte IDADE = userInput.nextByte();
 
         if (IDADE >= 18)
         {
-            System.out.println("Você é de maior. Vocẽ pode dirigir, beber whisky, gin e vodka :). O que eu não recomendo.");
+            System.out.println(NOME + ", você é de maior. Vocẽ pode dirigir, beber whisky, gin e vodka :). O que eu não recomendo.");
         } else{
-            System.out.println("Você é de menor. Aproveite enquanto seu tempo livre, estude mais.");
+            System.out.println(NOME + ", você é de menor. Aproveite enquanto seu tempo livre, estude mais.");
             System.out.println("Escola é overrated, não estude na escola. Estude em casa, melhore seu inglês.");
         }  
     }
