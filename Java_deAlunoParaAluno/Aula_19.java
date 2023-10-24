fd61839a8ecdf2ff58c837a802f9d33ab0bb3f52
Guilherme <Fireguiquenbr@gmail.com>
2023-10-24 19:32:18 -0300

    Update Aula_19.java

--- Java_deAlunoParaAluno/Aula_19.java
+++ Java_deAlunoParaAluno/Aula_19.java
@@ -8,15 +8,23 @@ public class Aula_19{
 
         /* 
             Por exemplo, utilizando apenas "if" e 'else' somos capazes de colocar uma mensagem para 
-            maiores de 18 e para menores de 18 anos.                                        */ 
+            maiores de 18 e para menores de 18 anos.                                        
+        */ 
         byte idade = 18;
-        if(idade >= 18){
+
+        if (idade >= 18){
             System.out.println("Você é de maior.");
         } else {
             System.out.println("Você é de menor.");
         }
 
-
         // Mas como vamos colocar uma mensagem especifica para quem colocar uma idade negativa? 
+        // Afinal, idade negativa não existe.
+        // Para isso, NÃO DEVEMOS ADICIONAR MAIS UM "IF" AO CODIGO, utilizaremos a junção de "else if".
+
+        if (age < 1){
+            System.out.println("Idade inserida não é válida.");
+        } 
+        else if ()
     }
 }
