0d3bd0c974b31f6dc1987a01917a5ce94c8a6668
Guilherme <Fireguiquenbr@gmail.com>
2023-10-23 23:02:21 -0300

    Update Aula_17.java

--- Java_deAlunoParaAluno/Aula_17.java
+++ Java_deAlunoParaAluno/Aula_17.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 public class Aula_17{
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
+
+
         // Define uma string. Não é um tipo de dado primitivo, é um objeto. 
         String nome = "Osvaldo";
-
         System.out.println(nome);
+        // ouput: "Osvaldo"
+
+
+        // Pegando uma entrada de informação. O usuário irá digitar seu nome no terminal. 
+        System.out.print("Digite seu nome: ");
+        String userName = input.next();
+
+        // printa o nome do usuário 
+        System.out.println("Olá, " + userName + "!");
 
     }
 }
