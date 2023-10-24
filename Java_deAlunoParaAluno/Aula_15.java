27c74a3cb4d3df8799754714e3b680cdf24ecf95
Guilherme <Fireguiquenbr@gmail.com>
2023-10-23 21:19:17 -0300

    Update Aula_15.java

--- Java_deAlunoParaAluno/Aula_15.java
+++ Java_deAlunoParaAluno/Aula_15.java
@@ -22,15 +22,25 @@ public class Aula_15{
 
         // Com este número, podemos saber quantos minutos, horas, dias, anos e etc se passaram.
         
-        // Para saber os segundos, basta dividir os milisegundos por 1000. Pois 1sec = 1000milisec
+        // Para saber os segundos totais passados, basta dividir os milisegundos por 1000. Pois 1sec = 1000milisec
         long totalSegundos = totalMilisegundos / 1000; 
 
-        // Para saber os minutos, basta dividir os segundos por 60. Pois 1min = 60s
+        // Para saber os minutos totais passados, basta dividir os segundos por 60. Pois 1min = 60s
         long totalMinutos = totalSegundos / 60; 
 
         // Para saber as horas totais, basta dividir os minutos por 60. Pois 1h = 60min 
         long totalHoras = totalMinutos / 60; 
 
-        // Para saber 
+        // Para saber os dias totais, basta dividir as horas por 24. Pois 1d = 24h 
+        long totalDias = totalHoras / 24; 
+
+        // Para saber os meses totais, basta dividir os dias por 30. Pois 1m = 30d 
+        // o número decimal 417 é para termos mais precisão. Fórmula via google.  
+        long totalMeses = (long) (totalDias / 30.417); 
+
+        // Para saber os anos totais, basta dividir os meses por 12. Pois 1a = 12m 
+        long totalAnos = (int)totalMeses / 12; 
+
+        System.out.println(totalAnos);
     }
 }
