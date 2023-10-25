559bdef9b24b9ad671d3b4a6f2e6e7de934469ff
Guilherme <Fireguiquenbr@gmail.com>
2023-10-25 18:08:14 -0300

    Update CalculadoraIMC.java

---
+++ Java_deAlunoParaAluno/exercicios/CalculadoraIMC.java
@@ -0,0 +1,16 @@
+import java.util.*; 
+
+public class CalculadoraIMC{
+    public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Digite seu peso (kg): ");
+        final short peso = userInput.nextInt();
+
+        System.out.println("Digite sua altura (cm): ");
+        final short altura = userInput.nextInt();
+
+        final byte =  
+
+    }
+}
