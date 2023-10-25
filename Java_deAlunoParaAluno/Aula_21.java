b2887210e4794c470ccbff1922174fc6d20dfa91
Guilherme <Fireguiquenbr@gmail.com>
2023-10-25 17:55:22 -0300

    Update Aula_21.java

--- Java_deAlunoParaAluno/Aula_21.java
+++ Java_deAlunoParaAluno/Aula_21.java
@@ -1,13 +1,4 @@
 // EXERCICIO: Ano bissexto
-// REGRAS:
-/*
-    1. Se o ano for uniformemente divisível por 4, vá para a etapa 2. Caso contrário, vá para a etapa 5.
-    2. Se o ano for uniformemente divisível por 100, vá para a etapa 3. Caso contrário, vá para a etapa 4.
-    3. Se o ano for uniformemente divisível por 400, vá para a etapa 4. Caso contrário, vá para a etapa 5.
-
-    4. O ano é bissexto (tem 366 dias);
-    5. O ano não é um ano bissexto (tem 365 dias).
-*/
 
 import java.util.Scanner;
 
