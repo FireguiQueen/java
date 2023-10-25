fe19504062c1ae4b2764a9f9b52131d5dd92dfa4
Guilherme <Fireguiquenbr@gmail.com>
2023-10-25 11:34:08 -0300

    Update Aula_20.java

--- Java_deAlunoParaAluno/Aula_20.java
+++ Java_deAlunoParaAluno/Aula_20.java
@@ -1,13 +1,23 @@
 // Operadores lógicos
 
+/*
+    &&: operador lógico "and". Ele requerer que duas ou mais expressões booleanas sejam TRUE.  
+    ||: operador lógico "or". Ele quer que apenas UMA das expressões booleanas seja TRUE. 
+*/
+
 public class Aula_19{
     public static void main(String[] args){
-        if(true){
-            System.out.println("oi1");
-        } if (false){
-            System.out.println("oi2");
-        } else {
-            System.out.println("oi3");
+        // Exemplo com && 
+        int num01 = 50;
+        int num02 = 20; 
+
+        // "A variável num01 é maior do que 30  E (and)  a variável num02 é maior do que 10?"
+        // Ou seja, para este bloco ser executado, deve haver dois "TRUE" para esta pergunta.
+        // Então o 'num01' precisa ser maior do que 30 E o 'num02' precisa ser maior do que 10. 
+        if (num01 > 30 && num02 > 10){
+            System.out.println(" :) ");
         }
-    }
+
+
+        
 }
