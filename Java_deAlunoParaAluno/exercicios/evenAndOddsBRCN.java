455a4687ad4ccf280bc8ea2f54c5760dd145ec08
Guilherme <Fireguiquenbr@gmail.com>
2023-10-26 19:41:06 -0300

    Update evenAndOddsBRCN.java

--- Java_deAlunoParaAluno/exercicios/evenAndOddsBRCN.java
+++ Java_deAlunoParaAluno/exercicios/evenAndOddsBRCN.java
@@ -2,7 +2,33 @@ import java.util.Scanner;
 
 public class evenAndOddsBRCN{
     public static void main(String[] args){
+        Scanner userInput = new Scanner(System.in);
         
-                
+
+        
+        int numero;
+        do {
+        System.out.print("Digite um número inteiro positivo: "); 
+        numero = userInput.nextInt();
+        } while (numero <= 0);
+
+        // irá imprimir apenas números pares. Excluindo números multiplos de 7. 
+
+        for (int i = 1; i <= numero; i++)
+        {
+            if (i % 10 == 0)
+            {
+                break;
+            }
+            
+            else if (i % 2 == 0 && i % 7 != 0)
+            {
+                System.out.println(i);
+                continue;
+            } 
+
+        }
+
+
     }
 }
