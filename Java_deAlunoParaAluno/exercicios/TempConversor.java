a3083fcc49c9d8f15a5c3fa117f4ba2f6a9acc06
Guilherme <Fireguiquenbr@gmail.com>
2023-10-26 11:50:08 -0300

    Update TempConversor.java

--- Java_deAlunoParaAluno/exercicios/TempConversor.java
+++ Java_deAlunoParaAluno/exercicios/TempConversor.java
@@ -9,25 +9,30 @@ public class TempConversor{
         System.out.println("1. Converter de Celsius para Fahrenheit;)"); 
         System.out.println("2. Converter de Fahrenheit para Celcius.");
     
+
+        byte conversor;
         do {
         System.out.print("-> ");
-        byte conversor = userInput.nextByte();
+        conversor = userInput.nextByte();
         } 
-        while (conversor >= 3 && conversor <= 0 )
+        while (conversor >= 3 || conversor <= 0 );
 
 
         if (conversor == 1)
         {
             System.out.print("Digite a temperatura em Celcius: ");
-            int temperatura = userInput.nextInt(); 
-            System.out.println(((temperatura * 9) / 5) + 32 );
-        } else {
-            // ( °F − 32) × 5/9 
+            final int temperatura = userInput.nextInt(); 
+            final int FAHRENHEIT = temperatura * 9 / 5 + 32;
+
+            System.out.println("Fahrenheit: " + FAHRENHEIT);
+        } 
+        else {
             System.out.print("Digite a temperatura em Fahrenheit: ");
-            int temperatura = userInput.nextInt(); 
-            System.out.println(((temperatura - 32) * 5) / 9);
+            final int temperatura = userInput.nextInt(); 
+            final int CELCIUS = ((temperatura - 32) * 5) / 9;
+
+            System.out.println("Celcius: " + CELCIUS);
         }
   
-
     }
 }
