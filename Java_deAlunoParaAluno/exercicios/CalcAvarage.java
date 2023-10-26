b4366a7a55752f01397843ffbeb751e5fcf339d8
Guilherme <Fireguiquenbr@gmail.com>
2023-10-26 19:59:46 -0300

    update

---
+++ Java_deAlunoParaAluno/exercicios/CalcAvarage.java
@@ -0,0 +1,21 @@
+// Exercício: Calculadora de Média com Interrupção
+
+/*
+    Crie um programa que solicita ao usuário para inserir uma série de notas de alunos. O programa deve calcular a média dessas notas. No entanto, o programa deve permitir que o usuário insira quantas notas desejar, mas o usuário pode interromper a entrada de notas a qualquer momento digitando um valor especial, como -1.
+
+    O programa deve calcular a média das notas inseridas até o momento.
+    O programa deve ignorar notas negativas (incluindo -1) ao calcular a média.
+    O programa deve interromper a entrada de notas e exibir a média quando o usuário inserir o valor especial (-1).
+
+Aqui está um exemplo de saída do programa:
+    Digite as notas dos alunos (para encerrar, digite -1):
+        Nota 1: 8
+        Nota 2: 7
+        Nota 3: 9
+        Nota 4: -1
+        Média: 8.0
+
+    Neste exercício, você pode usar um loop para continuar solicitando notas ao usuário e calcular a média. 
+    Utilize uma condição para verificar se a nota inserida é igual a -1 e, se for, interrompa o loop usando 
+    break. Use continue para ignorar notas negativas na hora do cálculo da média.
+*/
