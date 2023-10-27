dee1e65fba7a1b8ed364003c54c4e4e20ba644df
Guilherme <Fireguiquenbr@gmail.com>
2023-10-26 22:07:03 -0300

    Create Aula_28.java

---
+++ Java_deAlunoParaAluno/Aula_28.java
@@ -0,0 +1,19 @@
+// OPERADOR CONDICONAL 
+
+/*
+    O operador condicional em muitas vezes pode substituir estruturas condicionais que possuem apenas
+    um 'if - else'.
+
+    Primeiro, vamos colocar uma expressão que será avaliada em 'TRUE' ou 'FALSE'.
+    Após a expressão, colocaremos um ponto de interrogação.
+    Ao lado dele, teremos o que será feito caso a expressão seja "TRUE".
+    Após isso, teremos dois pontos (:), e outra parte, determinando o que será feito casa a expressão 
+    seja "FALSE".
+*/ 
+
+public class Aula_28{
+    public static void main(String[] args)
+    {
+        
+    }
+}
