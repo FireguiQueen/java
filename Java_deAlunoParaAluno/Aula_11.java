2661742071fa113db83003867f29ea99b613737c
Guilherme <Fireguiquenbr@gmail.com>
2023-10-29 23:36:35 -0300

    Update Aula_11.java

--- Java_deAlunoParaAluno/Aula_11.java
+++ Java_deAlunoParaAluno/Aula_11.java
@@ -1,7 +1,7 @@
 // Interação com o usuário: INPUTS 
 
-// Precisamos importar a classe "Scanner" para utilizar ela
-// É ela quem nos permite realizar entradas de informação.  
+// Precisamos importar a classe "Scanner" para utilizar ela (óbvio).
+// É ela quem nos permite receber entradas de informação.  
 import java.util.Scanner;
 
 public class Aula_11 {
@@ -10,31 +10,27 @@ public class Aula_11 {
 
         // Essa linha certamente está um pouco confusa a você, mas ignore ela por enquanto. 
         // Apenas entenda que esse é um jeito para você herdar comandos que irão trazer novas funcionalidades
-        // ao nosso programa.
-        Scanner entradaDeInformacao = new Scanner(System.in);
+        // ao nosso programa. Esses comandos ficarão salvos na variável 'input'.
+        Scanner input = new Scanner(System.in);
 
-        // Na aula 09 fizemos um programa que calcula a área de um triângulo. Mas todos os valores,
-        // como o da base e altura, foram definidos por nós pelo próprio código.
-
-        // Mas agora através dessa classe "Scanner", podemos receber valores do usuário via terminal. 
+        // Na aula 09, calculamos a área de um triângulo com valores definidos no código.
+        // Agora, vamos permitir que o usuário insira os valores.
         System.out.print("Digite o valor da base: ");
-        double base = entradaDeInformacao.nextDouble();
+        double base = input.nextDouble();
 
         System.out.print("Digite o valor da altura: ");
-        double high = entradaDeInformacao.nextDouble();
+        double high = input.nextDouble();
  
         double trianguleArea = (base * high) / 2;
 
         System.out.println("A área do triângulo é de: " + trianguleArea);
 
 
-
-
-
-        /* Vale lembrar que quando temos uma variável que recebe uma função por exemplo, primeiro é executado
-        toda a função, e depois, o valor retornado por ela é salvo na nossa variável. 
-        então as ações semprem ocrrem da direita para a esquerda. 
-        Primeiro a função é executada, e então aparece um lugar no terminal para o usuário digitar informações
+        /* Quando temos uma variável que recebe uma função, a primeira coisa que o ocorre é a função
+        ser executada, e depois, o valor retornado por ela é salvo na nossa variável. 
+        então as ações semprem ocorrem da direita para a esquerda. 
+        
+        > Primeiro a função é executada, e então aparece um lugar no terminal para o usuário digitar informações
         essas informações são enviadas para a função e ela faz um retorno. E este retorno fica salvo na variável. */
     }
 }
