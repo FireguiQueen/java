bb969cbdf1c1871bd998a5cbd6d50a58c1e84e5b
Guilherme <Fireguiquenbr@gmail.com>
2023-10-30 00:25:07 -0300

    Update Aula_20.java

--- Java_deAlunoParaAluno/Aula_20.java
+++ Java_deAlunoParaAluno/Aula_20.java
@@ -2,29 +2,42 @@
 
 /*
     &&: operador lógico "and". Ele requerer que duas ou mais expressões booleanas sejam TRUE.  
-    ||: operador lógico "or". Ele quer que apenas UMA das expressões booleanas seja TRUE. 
+    ||: operador lógico "or". Ele quer que APENAS UMA das expressões booleanas seja TRUE. 
 */
 
 public class Aula_19{
     public static void main(String[] args){
-        // Exemplo com && 
+        // EXEMPLOS COM: && 
         int num01 = 50;
         int num02 = 20; 
 
-        // "A variável num01 é maior do que 30  E (and)  a variável num02 é maior do que 10?"
+        // "variável num01 é maior do que 30   E (and)   variável num02 é maior do que 10?"
         // Ou seja, para este bloco ser executado, deve haver dois "TRUE" para esta pergunta.
-        // Então o 'num01' precisa ser maior do que 30 E o 'num02' precisa ser maior do que 10. 
+        // Então o 'num01' precisa ser maior do que 30  E  o 'num02' precisa ser maior do que 10. 
         if (num01 > 30 && num02 > 10){
             System.out.println(" :) ");
         }
 
+        // Este bloco será executado pois ambas as expressões são verdadeiras. 
+        // dez é maior que 0   AND  trinta é maior que zero.
+        if (10 > 0 && 30 > 0){
+            System.out.println(" :) ");
+        }
 
-        // Exempo com || 
+        // Este bloco não será executado pois apenas uma da expressão são verdadeiras.
+        // Para que ele seja executado, ambas precisam ser verdades. 
+        if (20 > 10 && 40 > 50){
+            System.out.println(" :) ");
+        }
+
+
+        // EXEMPLOS COM: || 
         int num03 = 20; 
         int num04 = 10; 
 
-        // "A varipavel num03 é maior do que 5   OU (or)  a variável num04 é maior do que 100?"
+        // "num03 é maior do que 5   || (or)  num04 é maior do que 100?"
         // Para este bloco ser executado, deve haver apenas um 'true' para esta pergunta.
+        
         // A variável 'num04' não é maior do que 100, mas a variável 'num03' é maior do que 5.. 
         // Como o "||" exige que apenas uma expressão seja verdadeira, o bloco será executado. 
         if (num03 > 5  ||  num04 > 100){
