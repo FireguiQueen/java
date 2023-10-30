2964e13d39beaaddd2e5132780af65204b986030
Guilherme <Fireguiquenbr@gmail.com>
2023-10-29 23:05:23 -0300

    Update Aula_04.java

--- Java_deAlunoParaAluno/Aula_04.java
+++ Java_deAlunoParaAluno/Aula_04.java
@@ -4,34 +4,37 @@ public class Aula_04{
     
     public static void main(String[] args){
         
-        // Isto é uma declaração (declaramos uma variável).  
-        // Para fazer uma declaração, colocamos o seu tipo de dado (int) e o seu nome (x).
+        // Esta é uma declaração (declaramos uma variável).
+        // Para fazer uma declaração, especificamos o seu tipo de dado (int) e o seu nome (x).
         // Como é do tipo "int", a variável aceita somente números INTEIROS!
-        int x; 
+        int x;
 
-        // Agora, podemos atruibuir um valor inteiro a ela;
-        // Usamos o seu nome, um sinal de atribuição de valor (=) e um valor.
+        // Agora, podemos atribuir um valor inteiro a ela;
+        // Esse processo é chamado de inicialização da variável e ocorre quando atribuímos um valor a ela.
+        // Usamos o seu nome, um sinal de atribuição (=) e um valor.
         x = 50;
 
-        // Mas também podemos fazer isto em uma única linha: 
+
+        // Mas também podemos fazer isto em uma única linha. Estamos declarando e inicializando ao mesmo tempo.
         // SINTAXE: tipo de dado - nomeDaVariavel - sinal de atribuição(=) - valor que desejamos (precisa estar de acordo com o tipo de dado)
         int y = 100; 
 
 
-        // Anteriomente usamos aspas duplas pois queriamos printar STRINGS.
-        // Mas agora, desejamos printar variáveis (pois elas são simplesmente um nome para se referir a um valor, seja ele
-        // primitivo ou não), sendo assim, precisamos colocar fora das aspas.
-        // Pois as aspas servem exclusivamente para strings.
-        
-        // Por exemplo, se utilizassemos aspas na linha abaixo, o CARACTER 'x' que seria printado, e não a variável.
-        System.out.println("x");
-        // resposta (output): "x"
+        // Usamos aspas duplas para imprimir STRINGS anteriormente.
+        // Agora, queremos imprimir variáveis. As variáveis são nomes para valores, sejam eles primitivos ou não.
+        // Portanto, colocamos as variáveis fora das aspas, já que as aspas são exclusivamente para strings.
+
+
+        int zz = 50;
+        // Por exemplo, se utilizássemos aspas na linha abaixo, o CARACTER 'z' seria impresso e não a variável.        
+        System.out.println("z");
+        // resposta (output): "z"
 
         // Sem aspas, estamos dizendo para printar a variável, ou seja, para o valor que ela aponta.
-        System.out.printin(x);
+        System.out.println(z);
         // output: "50"
 
-        // O valor que colocamos dentro dos paretensis na linha 31 é uma variável, sendo assim, estamos apenas nos referindo ao valor dela.
+        /*
         /* Porém, em nosso terminal, o será retornado mesmo é uma string. Então mesmo que você coloque um valor 
         inteiro, um valor quebrado ou qualquer outro, o que será retornado no terminal será STRING.  */
 
