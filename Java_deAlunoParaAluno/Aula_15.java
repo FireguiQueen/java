da881dc2a243929dd45d3c3ac72ce45cc0b35cef
Guilherme <Fireguiquenbr@gmail.com>
2023-10-29 23:55:49 -0300

    Update Aula_15.java

--- Java_deAlunoParaAluno/Aula_15.java
+++ Java_deAlunoParaAluno/Aula_15.java
@@ -4,7 +4,7 @@
     A hora Unix é uma forma de representar um carimbo de data/hora, representando a hora como o número 
     de segundos desde 1º de janeiro de 1970 às 00:00:00 UTC. 
     
-    Um dos principais benefícios de usar o  horário Unix é que ele pode ser representado como um número inteiro, 
+    Um dos principais benefícios de usar o horário Unix é que ele pode ser representado como um número inteiro, 
     facilitando a análise e o uso em diferentes sistemas.
 
     Resumidamente, é um valor inteiro em minutos ou segundos que representa o tempo passado desde jan 01, 1970 
