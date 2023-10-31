d208dc7c244eb51b334ed54a37cfa1df9e4a2af9
Guilherme <Fireguiquenbr@gmail.com>
2023-10-30 22:02:24 -0300

    Update Aula_31.java

--- Java_deAlunoParaAluno/Aula_31.java
+++ Java_deAlunoParaAluno/Aula_31.java
@@ -1,1 +1,10 @@
 // Arrays e laços de repetição
+
+public class Aula_31{
+    public static void main(String[] args)
+    {
+        // Na aula 30 vimos que é possivel "declarar" um array e só depois colocar os elementos.
+        // Bem, isso na verdade, é bem útil em alguns casos. 
+        
+    }
+}
