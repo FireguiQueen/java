e7a1fb20e2856590e6dc6930f8bbc1ae95f70dba
Guilherme <Fireguiquenbr@gmail.com>
2023-10-31 11:38:57 -0300

    Update Aula_32.java

--- Java_deAlunoParaAluno/Aula_32.java
+++ Java_deAlunoParaAluno/Aula_32.java
@@ -1,2 +1,26 @@
-// Bubble sort
+// Arrays II
+
+public class Aula_32{
+    public static void main(String[] args)
+    {
+        // Definimos uma variável que podem conter até 5 elementos.
+        int[] x = new int[5];
+
+        // Mas observe que ainda não definimos os elementos. 
+        // E o que aconteceria se printarmos um elemento que não foi definido?
+
+        // Por exemplo:
+        System.out.println(x[0]); // printa o indice 0 (primeiro elemento). 
+
+        // Mas o indice 0 ainda não foi definido. 
+        // Então, o output será: 0.
+
+        // Quando não definimos o elemento, o valor, por padrão, será sempre 0. 
+
+
+        
+
+    }
+}
+
 
