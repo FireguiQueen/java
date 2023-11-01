6de40ecba3cce8c4fe69ee8dea78c8e108aeb438
Guilherme <Fireguiquenbr@gmail.com>
2023-11-01 12:48:27 -0300

    Update Aula_33.java

---
+++ Java_deAlunoParaAluno/Aula_33.java
@@ -0,0 +1,28 @@
+// Bubble sort
+/*
+    O que é?
+    O bubble sort é um algoritimo para você organizar seu vetor numérico (byte, long, double..) tanto
+    em forma decrescente, quanto na forma crescente. 
+
+    Em java, e em outras diversas linguagens, já existem métodos prontos para ordernar
+    seu array, porém, é um algorítimo importante de se conhecer.
+*/
+
+public class Aula_33
+{
+    public static void main(String[] args)
+    {
+        // Definimos o array que desejamos organizar.
+        // O array contém 6 elementos. Ou seja, vai do indice 0 a 5. 
+        /*
+            ->> Sendo:  
+            indice[0] = primeiro elemento (5)
+            indice[1] = primeiro elemento (3)
+            indice[2] = primeiro elemento (1)
+            indice[3] = primeiro elemento (4)
+            indice[4] = primeiro elemento (6)
+            indice[5] = primeiro elemento (2)
+        */
+        int[] numeros = {5, 3, 1, 4, 6, 2};
+    }
+}
