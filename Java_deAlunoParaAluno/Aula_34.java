3d526c2f720e6cece6d0d820a7d1adeae7ad706a
Guilherme <Fireguiquenbr@gmail.com>
2023-11-03 21:15:23 -0300

    Update Aula_34.java

--- Java_deAlunoParaAluno/Aula_34.java
+++ Java_deAlunoParaAluno/Aula_34.java
@@ -4,12 +4,12 @@
     Ou seja, o elemento de um vetor multidimensional é um vetor. 
 
     // DEFININDO E ACESSANDO VALORES DE UM VETOR NORMAL. 
-    int[] lista = {100, 200, 300};
+    int[] lista = {100, 200, 300}; (definimos um array e colocamos seus elementos diretamente)
+            ! ou também
+    int[] lista = new int[3]; (definimos um array e não colocamos seus elementos, apenas definimos quantos elemenetos podem haver)
 
     lista[0];       --> retorna 100. 
     lista[2];       --> retorna 300. 
 
-
-    >> Com os vetores multidimensionais, podemos armazenar vetores dentro de vetores.
 
     // Como declarar eles
