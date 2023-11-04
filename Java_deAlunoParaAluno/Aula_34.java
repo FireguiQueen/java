7233dc6088989e3a34439ce57be173fb1d7abbd4
Guilherme <Fireguiquenbr@gmail.com>
2023-11-03 21:31:45 -0300

    Update Aula_34.java

--- Java_deAlunoParaAluno/Aula_34.java
+++ Java_deAlunoParaAluno/Aula_34.java
@@ -4,26 +4,37 @@
     Ou seja, o elemento de um vetor multidimensional é um vetor. 
 */
 
-    // >>> DEFININDO E ACESSANDO VALORES DE UM VETOR NORMAL.
+public class Aula_32
+{
+    public static void main(String[] args)
+    {
 
-    // 1º - Definimos um array e colocamos seus elementos diretamente.
-        int[] lista = {100, 200, 300}; 
-    
-    // 2° - Definimos um array e não colocamos seus elementos, apenas definimos quantos elemenetos podem haver
-        int[] lista_0 = new int[3];
+        // >>> DEFININDO E ACESSANDO VALORES DE UM VETOR NORMAL.
 
-    // Acessando seus valores:
-    System.out.println(lista[0]);  // retorna 100. 
-    System.out.println(lista[2]);  // retorna 300. 
+        // 1º - Definimos um array e colocamos seus elementos diretamente.
+            int[] lista = {100, 200, 300}; 
+
+        // 2° - Definimos um array e não colocamos seus elementos, apenas definimos quantos elemenetos podem haver
+            int[] lista_0 = new int[3];
+
+        // Acessando seus valores:
+        System.out.println(lista[0]);  // retorna 100. 
+        System.out.println(lista[2]);  // retorna 300. 
 
 
-    // >>> DEFININDO E ACESSANDO VALORES DE UM VETOR MULTIDIMENSIONAL.
+        // >>> DEFININDO E ACESSANDO VALORES DE UM VETOR MULTIDIMENSIONAL.
 
-    // 1° - Definimos um array multidimensional colocando seus elementos diretamente.
-    int[][] list = { [200, 100], [400, 2, 4], [0] };
+        // 1° - Definimos um array multidimensional colocando seus elementos diretamente.
+        int[][] list = { [200, 100], [400, 2, 4], [0] };
 
-    // Acessando seus valores:
-    
-    // Retorna 200.
-    // Entramos no array 'list'. Entramos no primeiro elemento dele (um vetor). Entramos no primeiro elemento deste vetor.
-    list[0][0]; 
+        // Acessando seus valores:
+
+        // Retorna 200.
+        // Entramos no array 'list'. Entramos no primeiro elemento dele (um vetor). Entramos no primeiro elemento deste vetor.
+        System.out.println(list[0][0]); 
+
+        // Retorna 4.
+        // Entramos no array 'list'. Entramos no segimdp elemento dele (um vetor). Entramos no terceiro elemento deste vetor.
+        System.out.println(list[1][2]); 
+    }
+}   
