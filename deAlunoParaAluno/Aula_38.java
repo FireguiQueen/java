48779eda924323d7599e7fbd74a70f2001273fbe
Guilherme <Fireguiquenbr@gmail.com>
2023-11-05 17:41:26 -0300

    Update Aula_38.java

--- deAlunoParaAluno/Aula_38.java
+++ deAlunoParaAluno/Aula_38.java
@@ -4,6 +4,14 @@ public class Aula_38
 {
     public static void main(String[] args)
     {
+        /* 
+            Para esta explicação, criaremos um programa onde seu objetivo é perguntar ao usuário a base 
+            e a altura de um triângulo, e iremos RETORNAR o tamanho dele. 
+        */ 
+        
+        // Para isto, iremos separar o nosso programa em DOIS MÉTODOS:
+        // 1. Receber informações de entrada (inputs)
+        // 2. Realizar o cálculo necessário e retornar ele. 
 
     }
 }
