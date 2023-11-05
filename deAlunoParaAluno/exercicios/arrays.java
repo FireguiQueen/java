64505d4af16fde2ac4ffec5c4243c11ae87f4d01
Guilherme <Fireguiquenbr@gmail.com>
2023-11-05 18:19:36 -0300

    Create arrays.java

---
+++ deAlunoParaAluno/exercicios/arrays.java
@@ -0,0 +1,37 @@
+public class arrays
+{
+    public static void main(String[] args)
+    {
+
+        // Construa um método capaz de iterar sobre uma lista de nomes.
+        String[] nomes = {"Guilherme", "Flora", "Paulo", "Bruno"};
+        iterarArray(nomes);
+
+
+        // Construa um método capaz de iterar sobre arrays multidimensionais 
+        String[][] = escolasDiretores = { {"Vladimir", "Walter Merrison"}, {"Julia Avocado", "Mary Kauser"}};
+
+        // Construa um método capaz de retornar o último e o primeiro elemento de um array. 
+    }
+
+    public static void iterarArray(String[] nomes)
+    {
+        int arrayLength = nomes.length; 
+
+        for (int i = 0; i < arrayLength; i++)
+        {
+            System.out.println(nomes[i]);
+        }
+    } 
+
+    public static void iterarArrayM(String[][] escolas)
+    {
+        int arrayLength = escolas.length; 
+        
+        for (int i = 0; i < arrayLength; i++)
+        {
+            
+        }
+    }
+
+}
