e5aaf656e98477e5c818cd3ce71bbc04f1a8a186
Guilherme <Fireguiquenbr@gmail.com>
2023-11-05 18:30:45 -0300

    Update arrays.java

--- deAlunoParaAluno/exercicios/arrays.java
+++ deAlunoParaAluno/exercicios/arrays.java
@@ -9,9 +9,12 @@ public class arrays
 
 
         // Construa um método capaz de iterar sobre arrays multidimensionais 
-        String[][] = escolasDiretores = { {"Vladimir", "Walter Merrison"}, {"Julia Avocado", "Mary Kauser"}};
+        String[][] escolasDiretores = { {"Vladimir", "Walter Merrison"}, {"Julia Avocado", "Mary Kauser"}};
+        iterarArrayM(escolasDiretores);
 
         // Construa um método capaz de retornar o último e o primeiro elemento de um array. 
+        int[] list = {200, 100, 900, 13414, 131, 142};
+        primeiroUltimo(list);
     }
 
     public static void iterarArray(String[] nomes)
@@ -30,8 +33,18 @@ public class arrays
         
         for (int i = 0; i < arrayLength; i++)
         {
-            
+            for (int j = 0; j < escolas[i].length; j++)
+            {
+                System.out.println(escolas[i][j]);
+            }
         }
+    }
+
+    public static void primeiroUltimo(int[] numeros)
+    {
+        int arrayLength = numeros.length;
+        System.out.println("Primeiro elemento: " + numeros[0]); 
+        System.out.println("Último elemento: " + numeros[arrayLength - 1]);
     }
 
 }
