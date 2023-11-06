a9b0363da4999700028c2b9bc6c65b83b9621111
Guilherme <Fireguiquenbr@gmail.com>
2023-11-06 09:26:23 -0300

    Update Aula_43.java

--- deAlunoParaAluno/Aula_43.java
+++ deAlunoParaAluno/Aula_43.java
@@ -1,2 +1,4 @@
 // POO (Object-oriented programming - Programação Orientada a Objetos)
-
+/*
+    Programação orientada a objetos é um conceito, é um paradigma, um jeito 
+    padronizado para programar.
