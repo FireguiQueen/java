cfc0f2559553ba1c3cb717662f5d73c5a24016b8
Guilherme <Fireguiquenbr@gmail.com>
2023-11-05 21:18:08 -0300

    Create Aula_39.java

---
+++ deAlunoParaAluno/Aula_39.java
@@ -0,0 +1,44 @@
+// MÉTODOS IV: teoria 
+
+// Métodos, parâmetros e argumentos
+/*
+    Um método é um bloco que possui um nome próprio. Este bloco conterá diversas instruções.
+    Assim que este método é invocado, essas instruções serão executadas. 
+
+    Um método pode receber inputs (valores de entrada). Para isso, o método precisa possuir parâmetros,
+    e esses parâmetros são utilizados diretamente em nosso bloco. 
+    Quando invocarmos este método, passaremos os argumentos, que por sua vez, são valores que substituiram 
+    esses parâmetros que estão dentro do bloco.
+
+    Esses parâmetros são utilizados
+    É útil para:
+        - Efiência
+        - Beleza do código 
+        - Praticidade
+*/
+
+
+// Cabeçalho 
+/*
+    O cabeçalho, ou em inglês, "header", é a parte onde declaramos o nosso método.
+    Nele conterá:
+    - modificadores: public static
+    - tipo de retorno: void, int, double, string.. 
+    - nome do método: (qualquer nome que a gente quiser)
+    - parâmetros: (são as variáveis sem valores, que ao invocarmos os métodos, terão valores atrubuidos a elas devido aos argumentos)
+*/
+public static int retorna1()
+{
+    return 1; 
+}
+
+
+// Corpo do método / bloco 
+/*
+    O corpo do método nada mais é do que as instruções que são contidas dentro deste método.
+    Eles ficam entre duas chaves {}. 
+*/
+
+
+
+
