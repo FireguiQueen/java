de31319cb8510fb5983204aca94573e2e13e87f0
Guilherme <Fireguiquenbr@gmail.com>
2023-11-06 09:33:43 -0300

    Update Aula_43.java

--- deAlunoParaAluno/Aula_43.java
+++ deAlunoParaAluno/Aula_43.java
@@ -2,3 +2,37 @@
 /*
     Programação orientada a objetos é um conceito, é um paradigma, um jeito 
     padronizado para programar.
+    >> PARADIGMA: um exemplo que serve como modelo; padrão.
+
+    A ideia do POO é que tudo pode se tornar um objeto.
+    Na vida real, nem tudo são objetos. Por exemplo, uma pessoa NÃO é um objeto.
+    Mas na programação, para definirmos esta pessoa, podemos dizer que ela É um objeto.
+
+    Um objeto carrega consigo:
+    - Propriedades
+    - Valores dessas propriedades
+    - Ações que podem ser executadas com ele.
+
+
+    Vamos transformar uma pessoa em um objeto:
+
+    pessoa 01:
+    nome: joão
+    idade: 47
+    cor: branca
+    profissão: policial
+
+    // essas foram as propriedades e valores do nosso objeto. Por mais que na vida real uma pessoa não possa
+    ser considerada um objeto, praticamente tudo do mundo real, pode virar um objeto na programação.
+
+
+    No mundo real, uma caneca é um objeto. Vamos colocar ela em nosso código, vamos definir suas propriedades
+    e ações que podemos fazer com ela.
+
+    caneca:
+    cor: preta
+    tamanho: média 
+    ação: beber()
+    ação: esvaziar()
+    ação: encher()
+*/
