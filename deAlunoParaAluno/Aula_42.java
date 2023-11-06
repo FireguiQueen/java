c3a191ecb42426604c74283ebb93f15a2265e98f
Guilherme <Fireguiquenbr@gmail.com>
2023-11-06 09:23:56 -0300

    Update Aula_42.java

--- deAlunoParaAluno/Aula_42.java
+++ deAlunoParaAluno/Aula_42.java
@@ -13,12 +13,14 @@ public class Aula_42
         */ 
         int[] arrayInvertido = inverterArray(numeros);
 
+        // printando o nosso array invertido.
         for (int i = 0; i < numeros.length; i++)
         {
             System.out.println(arrayInvertido[i]);
         }
     }
 
+    // método responsável por inveter o array
     public static int[] inverterArray(int[] vetor)
     {
         int comprimentoVetor = vetor.length;
@@ -43,12 +45,38 @@ public class Aula_42
             Porque fizemos o comprimento -1?
                 Quando usamos o ".length" em um array, ele nos retorna a quantidade de elementos.
                 Se o array possui 5 elementos, ele retorna 5. 
-                Mas como trabalhamos com indices, 
+                Mas como trabalhamos com indices, não existe o indice '5', com 5 elementos em um array,
+                o indice máximo é o indice 4. 
 
-    
 
+            Depois colocamos nossa condição, perguntando se 'i' é menor do que a quantiade de elementos do array
+
+            PRIMEIRO CICLO     
+            for (int i = 0, j = comprimentoVetor - 1; i < comprimentoVetor; i++, j--)
+            {
+            vetorInvertido[0] = vetor[4];
+            }
+
+            SEGUNDO CICLO
+            for (int i = 1, j = 4 - 1; i < comprimentoVetor; i++, j--)
+            {
+            vetorInvertido[1] = vetor[3];
+            }
+
+            TERCEIRO CICLO
+            for (int i = 2, j = 3 - 1; i < comprimentoVetor; i++, j--)
+            {
+            vetorInvertido[2] = vetor[2];
+            }
+
+            QUARTO CICLO
+            for (int i = 3, j = 2 - 1; i < comprimentoVetor; i++, j--)
+            {
+            vetorInvertido[3] = vetor[1];
+            }            
 
         */
+
     }    
 
 
