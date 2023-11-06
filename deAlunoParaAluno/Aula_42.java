a1bd13662d996495275f451b4d5abd26f33e514c
Guilherme <Fireguiquenbr@gmail.com>
2023-11-05 22:18:20 -0300

    Update Aula_42.java

---
+++ deAlunoParaAluno/Aula_42.java
@@ -0,0 +1,20 @@
+public class Aula_42
+{
+    public static void main(String[] args)
+    {
+        int[] numeros = {500, 400, 300, 200, 100};
+        inverse(numeros);
+
+        int arrayLength = numeros.length; 
+        for (int i = 0; i < arrayLength; i++)
+        {
+            System.out.println(numeros[i]);
+        }
+    }
+
+    // metodo - receber um vetor de um tipo de dado primitivo - retornar este array invertido
+    public static void inverse(int[] x)
+    {
+        
+    }
+}
