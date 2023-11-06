0c7c48561fe51104f72b308e883d82603485aafd
Guilherme <Fireguiquenbr@gmail.com>
2023-11-05 22:30:52 -0300

    Update Aula_42.java

--- deAlunoParaAluno/Aula_42.java
+++ deAlunoParaAluno/Aula_42.java
@@ -15,6 +15,20 @@ public class Aula_42
     // metodo - receber um vetor de um tipo de dado primitivo - retornar este array invertido
     public static void inverse(int[] x)
     {
-        
+        int arrayLength = x.length; 
+
+        for (int i = 0; i < arrayLength; i++)
+        {
+            int swap;
+
+            for (int j = 0; j < (arrayLength - 1); j++)
+            {
+                swap = x[j+1]; // 400 
+                x[j+1] = x[j]; // indice 01 recebe 500;
+                x[j] = swap; 
+            }
+        }
+
+
     }
 }
