15fee6fd43d32c0b5c121773a4fcf476ec8c5c2f
Guilherme <Fireguiquenbr@gmail.com>
2023-11-06 10:34:09 -0300

    update

---
+++ deAlunoParaAluno/u_Aula_43.java
@@ -0,0 +1,15 @@
+package deAlunoParaAluno;
+
+public class u_Aula_43
+{
+    public static void main(String[] args)
+    {
+        // Definimos que uma variável do tipo objeto irá receber a nossa classe TV.
+        // O 'new' é responsável por fazer a instância, por puxar todas as propriedades e métodos de determinada classe.
+        // neste caso, dizemos que a variável recebe propriedades e métodos da classe TV.
+        TV minhaTelevisao = new TV();
+
+
+        System.out.println(minhaTelevisao.ligada);
+    }
+}
