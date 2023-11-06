11d565ea1b0a6e735d5c7e39208d70dc2270f3fc
Guilherme <Fireguiquenbr@gmail.com>
2023-11-05 21:58:32 -0300

    Update Aula_40_II.java

--- deAlunoParaAluno/Aula_40_II.java
+++ deAlunoParaAluno/Aula_40_II.java
@@ -14,16 +14,43 @@ public class Aula_40_II
         int[] numeros = {0, 10, 20};
 
 
-        // 
+        // A PARTIR DESTA LINHA, RECOMENDO QUE LEIA OS COMENTÁRIOS DOS MÉTODOS CRIADOS.
+
+        // Agora, vamos invocar nossos métodos e passar as variáveis como argumento.
+
+        // Este método recebe um número inteiro, ou seja, um dado primitivo.
+        /* A questão é que, quando trabalhamos com dados primitivos, o que é passado é o valor 
+        da nossa variável. Por exemplo, a variável 'numero' tem o valor de 100. 
+        Como passamos ela como argumento, o que de fato está sendo colocado dentro desses paretensis é o valor
+        100. */ 
+        numeroInteiro(numero);
+
+
+        // Este método recebe um vetor de inteiros, ou seja, um dado não-primitivo.
+        /* Quando trabalhamos com dados não-primitivos, o que é passado é seu local na memória RAM.
+        Quando criamos um vetor por exemplo ele fica salvo em algum lugar em nossa memória RAM, e por consequência
+        ele possui um endereço. Vamos SUPOR que o endereço do nosso vetor 'numeros' seja '342592'.
+        Então, o que de fato, foi passado como argumento, foi este valor: '342592'. */
+        numerosInteiros(numeros);
+
+
     }
+
 
     // Vamos criar dois métodos do tipo void, pois não queremos retornar nada em ambos. 
 
-    // Este será responsável por receber um número inteiro.
+    // Este será responsável por receber um número inteiro como argumento.
     public static void numeroInteiro(int x)
     {
         // Aqui, vamos dizer que o parâmetro 'num' será 500. 
         // Isto é, não importa qual valor que o 'num' recebeu dos argumentos. Agora 'num' vale 500.
         x = 500;
+    }
+
+
+    // Este será responsável por receber um array de números inteiros como argumento.
+    public static void numerosInteiros(int[] x)
+    {
+        x[0] = 50; 
     }
 }
