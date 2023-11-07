4219cd11cbf7d5bd16bcdf95426159cf12f3ca94
Guilherme <Fireguiquenbr@gmail.com>
2023-11-07 00:16:38 -0300

    Update escola.java

--- deAlunoParaAluno/Aula_44/escola.java
+++ deAlunoParaAluno/Aula_44/escola.java
@@ -1,4 +1,8 @@
 public class carro
 {
 
+    // Mesmo que a gente NÃO defina. sempre haverá um construtor vazio.
+    // Um construtor não deve conter palavras-chave de retorno: void, int, long.. 
+    // Pois caso ele contenha, ele não será mais um CONSTRUTOR e sim um MÉTODO.
+    public carro() {}
 }
