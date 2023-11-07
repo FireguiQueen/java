f3a3ab388280af061b3c74a9fc0a07f735047a19
Guilherme <Fireguiquenbr@gmail.com>
2023-11-06 23:36:01 -0300

    update

--- deAlunoParaAluno/Aula_44/programa.java
+++ deAlunoParaAluno/Aula_44/programa.java
@@ -1,1 +1,18 @@
-// DIFERENÇA ENTRE CLASSE E MÉTODO.
+// DIFERENÇA ENTRE CLASSE E OBJETO
+
+/*
+    Isto será um resumo. Caso queira a explicação completa, vá para a aula 43. 
+
+    Uma classe é um molde.
+    Um objeto utiliza de uma classe para ter seus próprios valores.
+
+    Em um contexto fora da programação, podemos pensar em uma cidade.
+    Esta cidade possui escolas. As escolas são classes.
+
+    E as escolas desta cidade: 
+    - Thomas Jefferson High School
+    - Academic Magnet High Schoo
+    - Signature School
+
+    >> São objetos.
+*/ 
