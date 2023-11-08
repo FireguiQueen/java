79690952c93c78fd3655442d0c0e944b5fe55315
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 20:25:26 -0300

    Update Aula_05.java

--- Cursos/deAlunoParaAluno/Aula_05.java
+++ Cursos/deAlunoParaAluno/Aula_05.java
@@ -2,7 +2,6 @@
 
 class Aula_05
 {
-
     public static void main(String[] args){
 
         int x = 50;
@@ -13,31 +12,34 @@ class Aula_05
         int total = x + y; 
 
         System.out.println(total);
-        // resposta (output): 100;  
+        // output: 100;
 
 
         // Mas é claro, existem outros operadores, como o de subtração (-), divisão (/), multiplicação (*)... 
         int divisao = x / y;
+        // output: 1
+
         int multiplicacao = x * y; 
-        
+        // output: 2500
 
         // Como printar x e y sem juntar seus valores? Por exemplo: "50 50"
 
-        // NÃO FAÇA ISSO.
+        // NÃO FAÇA ISTO:
         System.out.println(x + y);
-        // output: 100 
+        // output: 100
 
         // Precisamos colocar uma string no meio 
         System.out.println(x + " " + y);
+        // output: 50 50
 
         // Mas por qual motivo precisamos fazer isso?
         /*
-            A resposta é bem simples. 
             Ao fazer: System.out.println(x + y); 
             - significa que desejamos juntar os valores daquelas variáveis, e assim será retornado 100 (50 + 50).
 
             Quando colocamos as aspas no meio, basicamente estamos dizendo que o valor de X irá se juntar 
-            a uma string e a string irá se juntar ao valor de y. 
+            a uma string, e esta string irá se juntar ao valor de y.
+            Ou seja, na segunda abordagem, mandamos o valor de X para uma string e depois, adicionar o y a esta string.
         */
 
     }
