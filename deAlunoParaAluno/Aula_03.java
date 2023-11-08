5b04e9c08e168e1ba556dbd88ba0f7fbab7794b6
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 00:09:04 -0300

    Update Aula_03.java

--- deAlunoParaAluno/Aula_03.java
+++ deAlunoParaAluno/Aula_03.java
@@ -3,18 +3,22 @@
 class Aula_03{
 
     /*
-        Em Java, é essencial utilizar as barras duplas (//) para comentários de linha ou as barras com 
-        asteriscos (comentários de bloco) para incluir explicações ou notas que não fazem parte da linguagem. 
-        Caso contrário, o Java não entenderá como compilar essa parte específica do código e retornará um erro.
+        Em Java, é essencial utilizar os comentários. Podemos usar as barras duplas (//) para comentários de linha
+        ou as barras com asteriscos, para comentários de bloco.
 
-        Por exemplo, o que você está lendo agora, por não ser parte da linguagem, deve estar inserido em um 
-        comentário.
+        Utilizamos os comentários para incluir explicações do programa ou código; Para não enfrentar erros
+        tudo o que não pertence a sintaxe da linguagem, deve ser colocado em forma de comentário, como por exemplo,
+        o que você está lendo agora. Se isto não tivesse dentro de um comentário, o JVM não entenderá
+        como compilar essa parte específica do código e retornará um erro.
     */
 
 
+    // EXEMPLOS DE COMENTÁRIOS EXPLICATIVOS.
 
-    public static void main(String[] args){
-        System.out.println("Olá!");
-
+    // public static void main é o nosso método principal do programa.
+    public static void main(String[] args)
+    {
+        System.out.println("Olá!"); // printa "Olá" no terminal.
     }
+
 }
