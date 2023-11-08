c1c993b3e4cef1c57f2fbaa77198fde4092f7a0e
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 20:43:48 -0300

    Update Aula_06.java

--- Cursos/deAlunoParaAluno/Aula_06.java
+++ Cursos/deAlunoParaAluno/Aula_06.java
@@ -3,7 +3,7 @@
 class Aula_06{
     public static void main(String[] args){
 
-        // variáveis que armazenam números inteiros usam o tipo 'int'.
+        // Variáveis que armazenam números inteiros usam o tipo 'int'.
         // Esse tipo aceita APENAS valores INTEIROS.. Não tente armazenar outra coisa, haverá erros.
         int y = 10; 
 
@@ -22,7 +22,7 @@ class Aula_06{
         // O nosso output foi 37 e não 37.5, ou seja, a parte decimal do valor não apareceu.
 
         // Vemos também o desaparecimento dos números quebrados neste exemplo:
-        // O resultado correto é '0,02', mas o nosso output será 0.
+        // O resultado correto é '0,02', mas o nosso output será 0 (apenas o valor inteiro foi mostrad).
         System.out.println(4 / 150);
 
 
@@ -40,16 +40,20 @@ class Aula_06{
 
             Quando dizemos que uma variável do tipo 'double' irá armazenar a divisão de 150 por 4, estamos
             apenas dizendo ao Java que aquela variável irá SURPORTAR um valor quebrado. 
-            
-            Mas como o valor daquela variável foi um inteiro dividido por outro inteiro, 
-            o resultado é um inteiro.
-          
+
             Para obter resultados com casas decimais, pelo menos um dos operandos deve ser um número de 
             ponto flutuante.
         */
-        // PARA OBTER O RESULTADO CORRETO, UM DOS NÚMERO PRECISA SER DE PONTO FLUTUANTE.
+
+        // PARA OBTER O RESULTADO CORRETO, UM DOS VALORES PRECISA SER CONTER A PARTE DECIMAL.
         System.out.println(150 / 4.0);
         // output: 37.2 (resultado correto).
 
+        // Mas e como fazer isto com variáveis?
+        // Para isto, usaremos algo chamada casting. Não irei explicar o que é, pois abordarei futuramente na aula 12.
+        int jj = 50;
+        int ll = 20;
+        System.out.println( (double)jj / ll);
+        
     }
 }
