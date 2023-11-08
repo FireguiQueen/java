e35f418ca04fcb83a945ee7600fa9f2fb293e45f
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 20:26:45 -0300

    update

--- Cursos/deAlunoParaAluno/Aula_05.java
+++ Cursos/deAlunoParaAluno/Aula_05.java
@@ -1,46 +1,39 @@
-// VARIÁVEIS II & Operações básicas de matemática
+// VARIÁVEIS II & Operações Básicas de Matemática
 
-class Aula_05
-{
-    public static void main(String[] args){
+public class Aula_05 {
+    public static void main(String[] args) {
 
         int x = 50;
         int y = 50;
 
-        // Podemos atribuir a uma variável outras variáveis. 
-        // Como neste caso, onde estamos dizendo "a variávei ¨total¨ RECEBE a variável ¨x¨ + ¨y¨"
-        int total = x + y; 
+        // Podemos atribuir uma variável a outra variável.
+        // Neste caso, estamos dizendo "a variável 'total' RECEBE a soma das variáveis 'x' e 'y'."
+        int total = x + y;
 
         System.out.println(total);
-        // output: 100;
+        // Saída (output): 100
 
-
-        // Mas é claro, existem outros operadores, como o de subtração (-), divisão (/), multiplicação (*)... 
+        // Além da adição (+), existem outros operadores, como subtração (-), divisão (/) e multiplicação (*).
         int divisao = x / y;
-        // output: 1
+        // Saída (output): 1
 
-        int multiplicacao = x * y; 
-        // output: 2500
+        int multiplicacao = x * y;
+        // Saída (output): 2500
 
-        // Como printar x e y sem juntar seus valores? Por exemplo: "50 50"
+        // Como imprimir os valores de 'x' e 'y' separados por espaço, por exemplo, "50 50"?
 
-        // NÃO FAÇA ISTO:
+        // NÃO FAÇA ISSO:
         System.out.println(x + y);
-        // output: 100
+        // Saída (output): 100
 
-        // Precisamos colocar uma string no meio 
+        // Para fazer isso, é necessário concatenar uma string no meio.
         System.out.println(x + " " + y);
-        // output: 50 50
+        // Saída (output): "50 50"
 
-        // Mas por qual motivo precisamos fazer isso?
+        // Mas por que precisamos fazer isso?
         /*
-            Ao fazer: System.out.println(x + y); 
-            - significa que desejamos juntar os valores daquelas variáveis, e assim será retornado 100 (50 + 50).
-
-            Quando colocamos as aspas no meio, basicamente estamos dizendo que o valor de X irá se juntar 
-            a uma string, e esta string irá se juntar ao valor de y.
-            Ou seja, na segunda abordagem, mandamos o valor de X para uma string e depois, adicionar o y a esta string.
+        Quando usamos System.out.println(x + y), estamos pedindo para juntar (somar) os valores das variáveis, resultando em 100 (50 + 50).
+        Quando inserimos as aspas no meio (System.out.println(x + " " + y)), estamos informando que o valor de 'x' deve ser unido a uma string e, em seguida, o valor de 'y' será anexado a essa string. Portanto, na segunda abordagem, convertemos o valor de 'x' em uma string e depois acrescentamos o valor de 'y' a essa string.
         */
-
     }
 }
