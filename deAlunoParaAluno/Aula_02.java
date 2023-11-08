3b743d68beb20990d7c98129d0062718d382b2e5
Guilherme <Fireguiquenbr@gmail.com>
2023-11-07 23:42:11 -0300

    update ex 02

--- deAlunoParaAluno/Aula_02.java
+++ deAlunoParaAluno/Aula_02.java
@@ -1,18 +1,22 @@
-// ESTRUTURA 
+// ESTRUTURA
+/*
+    Neste ponto inicial da linguagem Java, vamos ignorar algumas palavras-chave, tais como:
+    public, static, void, String[]...
+*/
 
-/* Por enquanto, como estamos em um ponto bem inicial da linguagem java, iremos ignorar algumas palavras-chave.
-Tais como: public, static, void, String[]... */ 
-
-// Os colchetes representam um bloco. Tudo que é colocado dentro deles pertencem a nossa classe. 
-// A classe é uma estrutura responsável por armazenar atributos e métodos.
+// Os colchetes representam um bloco, um escopo. Tudo o que está dentro deles pertence à nossa classe.
 class Aula_02
 {
 
-    // Esse é o nosso método principal e ele informa o ponto de partida do nosso programa;
-    // Usamos a palavra-chave "main" justamente por isso, pois PRECISAMOS de um método PRINCIPAL. 
+    // Este é o nosso método principal e ele indica o PONTO DE PARTIDA do nosso programa;
+    // Usamos a palavra-chave "main" justamente para isso, porque PRECISAMOS de um método PRINCIPAL.
+    // Este método quem derá inicio a execução de todas as instruções de nosso programa.
+    // > Caso não coloquemos um método principal (main) esta será uma classe de molde utilizada para criar objetos.
+    // > Mas veremos isto mais pra frente.
     public static void main(String[] args){
 
-        // O "println" é o método responsável por printar uma mensagem em nosso terminal.
+        // O "println" é o método responsável por imprimir uma mensagem em nosso terminal.
+        // Dentro dos parênteses, passamos uma string que deve estar contida entre aspas duplas.
         System.out.println("Olá!");
     }
 }
