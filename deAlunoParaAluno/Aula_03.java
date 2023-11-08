b20ecdfc0fd370f725df20e9626e191326b197ef
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 00:12:25 -0300

    Update Aula_03.java

--- deAlunoParaAluno/Aula_03.java
+++ deAlunoParaAluno/Aula_03.java
@@ -1,24 +1,21 @@
 // COMENTÁRIOS
+/*
+    Em Java, é essencial utilizar comentários. Podemos usar barras duplas (//) para comentários de linha
+    ou barras com asteriscos para comentários de bloco.
 
-class Aula_03{
-
-    /*
-        Em Java, é essencial utilizar os comentários. Podemos usar as barras duplas (//) para comentários de linha
-        ou as barras com asteriscos, para comentários de bloco.
-
-        Utilizamos os comentários para incluir explicações do programa ou código; Para não enfrentar erros
-        tudo o que não pertence a sintaxe da linguagem, deve ser colocado em forma de comentário, como por exemplo,
-        o que você está lendo agora. Se isto não tivesse dentro de um comentário, o JVM não entenderá
-        como compilar essa parte específica do código e retornará um erro.
+    Usamos comentários para incluir explicações do programa ou de determinado trecho de código.
+    Para evitar erros, tudo o que não faz parte da sintaxe da linguagem deve ser colocado na forma de comentário,
+    como, por exemplo, o que você está lendo agora. Se isso não estivesse dentro de um comentário, a JVM não
+    entenderia como compilar essa parte específica do código e retornaria um erro.
     */
 
+// EXEMPLOS DE COMENTÁRIOS EXPLICATIVOS.
+class Aula_03 {
 
-    // EXEMPLOS DE COMENTÁRIOS EXPLICATIVOS.
-
-    // public static void main é o nosso método principal do programa.
+    // O método public static void main é o nosso método principal do programa.
     public static void main(String[] args)
     {
-        System.out.println("Olá!"); // printa "Olá" no terminal.
+        System.out.println("Olá!"); // imprime "Olá" no terminal.
     }
 
 }
