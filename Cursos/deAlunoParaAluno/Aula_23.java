0b0e9ce2049242255126eb2e238cc2a9455fb4d7
Guilherme <Fireguiquenbr@gmail.com>
2023-11-09 19:44:15 -0300

    Update Aula_23.java

--- Cursos/deAlunoParaAluno/Aula_23.java
+++ Cursos/deAlunoParaAluno/Aula_23.java
@@ -1,42 +1,39 @@
-// LOOP.. Laço de repetição.. Ciclo.. (é tudo a mesma coisa): DO WHILE 
+// LOOP.. Laço de repetição.. Ciclo.. (é tudo a mesma coisa)
 
-public class Aula_23{
-    public static void main(String[] args)
-    {
+import java.util.Scanner;
+
+public class Aula_23 {
+    public static void main(String[] args) {
         // DO WHILE (faça.. Enquanto.. )
-        /* 
+        /*
             O 'do while' é bem parecido com o 'while', a diferença é que no 'do while' sempre iremos começar
-            executando o bloco do 'do', independentemente se a condição for falsa ou verdadeira. 
+            executando o bloco do 'do', independentemente se a condição for falsa ou verdadeira.
 
-            no 'while', primeiro é analisado a condição, se ela for true, execute o bloco, se for false, 
-            não execute. 
+            No 'while', primeiro é analisada a condição. Se ela for true, execute o bloco. Se for false,
+            não execute.
         */
 
+        // Exemplo:
+        int x = 1;
 
-       // Exemplo:
-       int x = 1;
-       int y = 1; 
-
-        // observe que nossa condição é "faça isso enquanto X (1) for maior do que 10". 
-        // o X não é maior do que 10, mas como estamos usando o 'do while', o bloco será executado 
-        // independente da condição ser false ou verdadeira. 
+        // observe que nossa condição é "faça isso enquanto X (1) for maior do que 10".
+        // o X não é maior do que 10, mas como estamos usando o 'do while', o bloco será executado
+        // independentemente da condição ser false ou verdadeira.
         // Após o bloco do 'do' ser executado, teremos uma condição, se ela for verdadeira, o bloco 'do' será
-        // executado, se for falsa, sairemos deste laço de repetição. 
-        do{
+        // executado, se for falsa, sairemos deste laço de repetição.
+        do {
             System.out.println("Olá!!");
         } while (x > 10);
 
-
         // no 'while' a situação é diferente. É imposto uma condição, se ela for verdadeira, o bloco
-        // será executado, se for falsa, sairemos do laço. 
-        while (x > 10){
+        // será executado, se for falsa, sairemos do laço.
+        while (x > 10) {
             System.out.println("Olá!!");
         }
 
         // RESULTADO:
         // Apenas o bloco do "do while" será executado. E será executado apenas uma vez.
-        // O bloco do laço 'while' não será executado, pois a condição é falsa. 
-
+        // O bloco do laço 'while' não será executado, pois a condição é falsa.
 
         // Conclusão:
         /*
@@ -44,28 +41,25 @@ public class Aula_23{
             uma vez. Já que primeiro é executado e depois perguntado.
             A famosa frase "Atirar e depois perguntar"
 
-            No while, a frase fica: "Perguntar e depois atirar". 
-        */ 
+            No while, a frase fica: "Perguntar e depois atirar".
+        */
 
-       
-        // Quando o 'do while' pode ser utilizado ao invés do 'while'? 
+        // Quando o 'do while' pode ser utilizado ao invés do 'while'?
         /*
-            Imagine um programa bem simples que utiliza do terminal.
-            A ideia é o usuário digitar um número positivo e será retornado se o número é ímpar ou pár. 
-            Para isso, precisamos utilizar a classe "scanner" do java. 
+            Imagine um programa bem simples que utiliza o terminal.
+            A ideia é o usuário digitar um número positivo e será retornado se o número é ímpar ou par.
+            Para isso, precisamos utilizar a classe "Scanner" do Java.
 
-            Detalhe, queremos APENAS números POSITIVOS, então, podemos montar o seguinte algoritomo:
+            Detalhe, queremos APENAS números POSITIVOS, então, podemos montar o seguinte algoritmo:
 
             do {
                 int x = user.nextInt();
             } while (x < 0)
 
             Ou seja, primeiro aparecerá um campo para o usuário digitar um número inteiro.
-            Caso ele digite um número negativo, o input aparecerá novamente: pois o X será menor do que zero.
+            Caso ele digite um número negativo, o input aparecerá novamente, pois o X será menor do que zero.
 
-            Caso ele digite um número positivo, terminaremos este laço de repetição: 
-            pois X será maior do que zero.
-
+            Caso ele digite um número positivo, terminaremos este laço de repetição, pois X será maior do que zero.
         */
     }
 }
