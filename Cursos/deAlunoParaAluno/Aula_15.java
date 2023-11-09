5039a6bd492e6ad28bb8c31b86124468d49c8df6
Guilherme <Fireguiquenbr@gmail.com>
2023-11-09 18:42:13 -0300

    update

--- Cursos/deAlunoParaAluno/Aula_15.java
+++ Cursos/deAlunoParaAluno/Aula_15.java
@@ -6,14 +6,14 @@
     Um dos principais benefícios de usar o horário Unix é que ele pode ser representado como um número inteiro, 
     facilitando a análise e o uso em diferentes sistemas.
 
-    Resumidamente, é um valor inteiro em minutos ou segundos que representa o tempo passado desde jan 01, 1970 
-    até hoje.
+    Resumidamente, é um valor inteiro em segundos ou milisegundos que representa o
+    tempo passado desde jan 01, 1970 até hoje.
 */ 
 
 public class Aula_15{
     public static void main(String[] args)
     {
-        // Em java, podemos pegar os milisegundos através do método "currentTimeMillis()".
+        // Em java, podemos pegar esses milisegundos através do método "currentTimeMillis()".
         // Como o retorno é em milisegundos, é um valor inteiro muito grande, então precisamos usar o 'long' 
         long totalMilisegundos = System.currentTimeMillis();
         System.out.println(totalMilisegundos);
--- Cursos/deAlunoParaAluno/Aula_16.java
+++ Cursos/deAlunoParaAluno/Aula_16.java
@@ -1,8 +1,9 @@
 // SYNTATIC SUGAR
 /*
-    wikipedia:
-    um açúcar sintático é uma sintaxe dentro da linguagem de programação que tem
+    Wikipedia:
+    O 'açúcar sintático' é uma sintaxe dentro da linguagem de programação que tem
     por finalidade tornar suas construções mais fáceis de serem lidas e expressas.
+
     Ela faz com que o uso da linguagem se torne "mais doce" para o uso humano,
     permitindo que suas funcionalidades sejam expressas mais claramente,
     mais concisamente ou, ainda, como um estilo alternativo preferido por alguns.
@@ -14,7 +15,7 @@ public class Aula_16{
         // Definindo uma variável e um valor. 
         int x = 5; 
 
-        // Reatribuindo os valores.
+        // Reatribuindo os valores com a sintaxe padrão
         // -->> Vamos dizer que a variável 'x' recebe ela mesma mais +10.
         // > em outras palavras: x = 5 + 10;
         x = x + 10; 
@@ -23,7 +24,6 @@ public class Aula_16{
         // Isto significa "Atribua +10 ao valor atual de X".
         x += 10; 
 
-
         // Outro exemplo:
         int y = 100;
         y += 50;
@@ -31,29 +31,24 @@ public class Aula_16{
         // Neste exemplo acima, seria a mesma coisa do que fazer "y = y + 50".
 
 
-
-        // PODEMOS TAMBÉM UTILIZAR OUTROS OPERADORES: 
+        // PODEMOS TAMBÉM UTILIZAR OUTROS OPERADORES:
 
         /* Multiplicação */  
         int z = 10;
-        // z = z * 2
-        z *= 2;
+        z *= 2; // z = z * 2
 
         /* Divisão */
         int k = 100;
-        // k = k / 2
-        k /= 2;
+        k /= 2; // k = k / 2
 
         // Há também os operadores de incremento e decremento.
         // Onde literalmente adicionam ou removem '1' do nosso valor.
         int j = 0;
-        // j = j - 1
-        j--;
+        j--; // equivalente: j = j - 1
         // ouput: -1
 
         int n = 0;
-        // n = n + 1
-        n++;
+        n++; // n = n + 1
         // output: 1
     }
 }
