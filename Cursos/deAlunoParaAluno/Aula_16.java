fa2e005472116bb9914866a4b5b6bf94a9bfe5e3
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 23:19:18 -0300

    Update Aula_16.java

--- Cursos/deAlunoParaAluno/Aula_16.java
+++ Cursos/deAlunoParaAluno/Aula_16.java
@@ -14,44 +14,46 @@ public class Aula_16{
         // Definindo uma variável e um valor. 
         int x = 5; 
 
-        // Reatribuindo os valores, vamos dizer que a variável X recebe ela mesma mais +10. 
+        // Reatribuindo os valores.
+        // -->> Vamos dizer que a variável 'x' recebe ela mesma mais +10.
+        // > em outras palavras: x = 5 + 10;
         x = x + 10; 
 
-        // Mas há um jeito mais prático, nos possibilitando de simplificar a sintaxe. 
+        // Mas há um jeito mais prático, mais "doce", nos possibilitando de simplificar a sintaxe.
         // Isto significa "Atribua +10 ao valor atual de X".
         x += 10; 
 
 
         // Outro exemplo:
-        int y = 100; 
-        y += 50;        // é a mesma coisa do que fazer "y = y + 50".
-        // output: 150. 
+        int y = 100;
+        y += 50;
+        // output: 150.
+        // Neste exemplo acima, seria a mesma coisa do que fazer "y = y + 50".
+
 
 
         // PODEMOS TAMBÉM UTILIZAR OUTROS OPERADORES: 
 
         /* Multiplicação */  
-        int z = 10; 
-        z *= 2;         // z = z * 2
-        // output: 20; 
-
+        int z = 10;
+        // z = z * 2
+        z *= 2;
 
         /* Divisão */
-        int k = 100; 
-        k /= 2;         // k = k / 2
-        // output: 50; 
+        int k = 100;
+        // k = k / 2
+        k /= 2;
 
-
-        // Há também os operadores de incremento e decremento, onde literalmente adicionam ou removem o valor inteiro de '1'.
+        // Há também os operadores de incremento e decremento.
+        // Onde literalmente adicionam ou removem '1' do nosso valor.
         int j = 0;
-        j--;            // j = j - 1
+        // j = j - 1
+        j--;
         // ouput: -1
 
-        int n = 0; 
-        n++;            // n = n + 1
+        int n = 0;
+        // n = n + 1
+        n++;
         // output: 1
-
-
-
     }
 }
