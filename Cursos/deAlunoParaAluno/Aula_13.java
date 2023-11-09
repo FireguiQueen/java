c66d67f71508c353cc5f52d317e5bf4b2ad2946f
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 23:05:13 -0300

    Update Aula_13.java

--- Cursos/deAlunoParaAluno/Aula_13.java
+++ Cursos/deAlunoParaAluno/Aula_13.java
@@ -1,11 +1,11 @@
 // CONSTANTES
 /* 
-    Em java, podemos transformar nossas variáveis em constantes (ou seja, o valor NÃO poderá ser alterado);
+    Em Java, podemos transformar nossas variáveis em constantes (ou seja, o valor NÃO poderá ser alterado);
     Basta utilizar a palavra-chave 'final' antes do tipo de dado.
 
-    Utilizaremos constantes quando vejos que o valor sempre será fixo, e que não precisa sofrer alterações.
-    >> É uma boa prática os nomes das variáveis constantes serem em letras maiúsculas.   
-*/ 
+    Utilizaremos constantes quando sabemos que o valor sempre será fixo e não precisa sofrer alterações.
+    >> É uma boa prática que os nomes das variáveis constantes sejam escritos em letras maiúsculas.
+*/
 
 public class Aula_13{
     public static void main(String[] args)
@@ -13,8 +13,8 @@ public class Aula_13{
         final long CPF = 02133122750; // define uma constante do tipo 'long'.
         System.out.println("CPF: " + CPF);
 
-
-        // Tentar fazer isto retornará erros. Pois variáveis constantes não podem ter seus valores alterados.
+        
+        // Tentar fazer isso retornará erros. Variáveis constantes não podem ter seus valores alterados.
         /*
             CPF = 24324;
         */
