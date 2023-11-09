6ee78dcc71a72537d9593f4dd303e314fad65c19
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 22:44:30 -0300

    Update Aula_11.java

--- Cursos/deAlunoParaAluno/Aula_11.java
+++ Cursos/deAlunoParaAluno/Aula_11.java
@@ -1,7 +1,12 @@
-// Interação com o usuário: INPUTS 
+// INPUTS (entradas de informação)
+/*
+   A entrada de informações é uma parte fundamental em muitos programas. Ela nos permite coletar dados
+   fornecidos pelo usuário ou de outras fontes. Neste caso, usamos a classe "Scanner" para receber
+   entradas de informações do usuário.
+ */
 
 // Precisamos importar a classe "Scanner" para utilizar ela (óbvio).
-// É ela quem nos permite receber entradas de informação.  
+// É ela quem nos permite receber essas entradas de informação.
 import java.util.Scanner;
 
 public class Aula_11 {
@@ -9,8 +14,9 @@ public class Aula_11 {
     {
 
         // Essa linha certamente está um pouco confusa a você, mas ignore ela por enquanto. 
-        // Apenas entenda que esse é um jeito para você herdar comandos que irão trazer novas funcionalidades
-        // ao nosso programa. Esses comandos ficarão salvos na variável 'input'.
+        // Apenas saiba que esta é uma forma herdar instruções de outro arquivo, e que irão trazer
+        // novas funcionalidades ao nosso programa, como por exemplo, receber inputs via terminal.
+        // As instruções herdados ficarão salvos na variável 'input'.
         Scanner input = new Scanner(System.in);
 
         // Na aula 09, calculamos a área de um triângulo com valores definidos no código.
@@ -22,15 +28,18 @@ public class Aula_11 {
         double high = input.nextDouble();
  
         double trianguleArea = (base * high) / 2;
-
         System.out.println("A área do triângulo é de: " + trianguleArea);
 
+        /*
+            Quando temos uma variável que recebe um método:
+            double base = input.nextDouble();
 
-        /* Quando temos uma variável que recebe uma função, a primeira coisa que o ocorre é a função
-        ser executada, e depois, o valor retornado por ela é salvo na nossa variável. 
-        então as ações semprem ocorrem da direita para a esquerda. 
-        
-        > Primeiro a função é executada, e então aparece um lugar no terminal para o usuário digitar informações
-        essas informações são enviadas para a função e ela faz um retorno. E este retorno fica salvo na variável. */
+            A primeira coisa que ocorre é o método ser executado, e depois, o valor retornado
+            por ele é salvo na nossa variável.
+            -> Então, as ações ocorrem da direita para a esquerda.
+
+            -> Primeiro o método é executado, e depois aparece um local no terminal para o usuário digitar informações.
+            Essas informações são enviadas para o método e ele retorna um valor, que fica armazenado na variável.
+        */
     }
 }
