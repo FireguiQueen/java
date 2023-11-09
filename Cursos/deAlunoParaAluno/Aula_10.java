8d99ceb903e464420d3f2a41f1f5d47ae67abff8
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 22:35:10 -0300

    Update Aula_10.java

--- Cursos/deAlunoParaAluno/Aula_10.java
+++ Cursos/deAlunoParaAluno/Aula_10.java
@@ -1,19 +1,17 @@
 // IMPORTAÇÃO DE CLASSES
 /*
-    A instrução "import" permite a gente importar (trazer) classes e pacotes para o nosso programa.
+    A instrução "import" permite importar (trazer) classes e pacotes para o nosso programa.
 */
 
-
 // Neste caso, estamos importando a classe "Scanner" do pacote "java.util".
-// O 'scanner' é útil pois nos permite receber inputs do usuário.
+// O 'Scanner' é útil porque nos permite receber inputs do usuário.
 // Sem essa importação, não poderíamos usar a classe Scanner em nosso programa.
 import java.util.Scanner;
 
 // Para importar TODAS as classes de um pacote de uma vez, podemos usar o asterisco (*).
 import java.util.*;
 
-
-public class Aula_10{
+public class Aula_10 {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
