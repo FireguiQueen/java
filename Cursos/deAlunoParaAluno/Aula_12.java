8f5a2d42683a8191c2f0d5fd9f0a6afc864d8453
Guilherme <Fireguiquenbr@gmail.com>
2023-11-08 23:01:16 -0300

    Update Aula_12.java

--- Cursos/deAlunoParaAluno/Aula_12.java
+++ Cursos/deAlunoParaAluno/Aula_12.java
@@ -1,33 +1,33 @@
 // CASTING 
 /*
-    O casting é uma transformação aplicada em valores numéricos para modificar seu tipo
-    de dado.
+    O casting é uma transformação aplicada em valores numéricos para modificar seu tipo de dado.
     É o ato de forçarmos uma determinada variável ou valor a ser de um determinado tipo.
  */
 
 public class Aula_12{
     public static void main(String[] args)
     {
-        // Na aula 06 vimos que um número inteiro divido por outro número inteiro sempre resulturá em um valor inteiro.
+        // Na aula 06, vimos que a divisão de um número inteiro por outro número inteiro resulta em um valor inteiro.        /*
         /*
-            Isto é, 50 dividido por 3 é '16.6666..', ou seja, um valor quebrado. 
-            Porém, no java, se realizarmos este cálculo (50/3), teremos como resultado, teremos um valor inteiro
+            Por exemplo, 50 dividido por 3 é igual a '16.6666...', um valor com casas decimais.
+            No entanto, em Java, se realizarmos esse cálculo (50/3), o resultado será um valor inteiro,
             que não contém partes decimais.
         */
         System.out.println(50 / 3); 
         // output: 16. 
 
-        // E como vimos na aula 06, para obtermos o resultado correto, ou seja, o valor '16.6666..', um dos
-        // números inteiros deve ser um número de ponto flutuante, e podemos fazer isto através do 'casting'.
-        
-        // Basta escolher um tipo de dado e colocar em paretensis ao lado do número que você deseja converter
+        // Como vimos na aula 06, para obter o resultado correto, ou seja, o valor '16.6666...', um dos
+        // números deve ser um número de ponto flutuante, e podemos fazer isso usando o "casting".
+
+        // Basta escolher um tipo de dado e colocá-lo entre parênteses ao lado do número que você deseja converter.
         System.out.println(50 / (float) 3);
         // output: 16.6666
 
-        // Mas como nessa situação estamos trabalhando sem variáveis, podemos simplesmente colocar um .0 ao lado 
-        // de um número inteiro. Isto não é casting, estamos apenas dividindo um valor inteiro por um valor de ponto flutuante.
+        // Nessa situação, como estamos trabalhando com valores literais, podemos simplesmente colocar um ".0" ao lado
+        // de um número inteiro. Isso não é casting, estamos apenas dividindo um valor inteiro por um valor de ponto flutuante.
         System.out.println(50 / 3.0);
         // output: 16.6666
+        
 
 
         // Por última análise, podemos ver que casting fazendo efeito.
