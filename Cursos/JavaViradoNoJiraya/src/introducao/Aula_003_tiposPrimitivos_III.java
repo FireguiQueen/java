63fb68908666f4e2d43060d20a2b6988007ba687
Guilherme <Fireguiquenbr@gmail.com>
2023-11-13 19:47:25 -0300

    Update Aula_003_tiposPrimitivos_III.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_003_tiposPrimitivos_III.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_003_tiposPrimitivos_III.java
@@ -1,4 +1,4 @@
-// CASTING & tipos-primitivos para decimais
+// CASTING & valores tipos-primitivos e seus tipos
 /*
     O casting é uma conversão forçada entre valores numéricos.
     Ou seja, temos um valor de um determinado tipo e, através do 'casting', forçamos
@@ -29,42 +29,50 @@ public class Aula_003_tiposPrimitivos_III
         // Primeiramente, o valor '500.20' é convertido para um número inteiro por meio do casting.
         // Assim, o valor resultante é armazenado na variável.
 
+
         // *** * * * * * * *** ** * * * * * * * ** * * * * * * *** * * * * * *
 
         // DOUBLE VS FLOAT
         /*
-          O double e o float são semelhantes, mas a diferença está na quantidade de
-          bytes utilizados para armazenar valores.
-          Um 'double' é capaz de armazenar números decimais maiores.
+            O double e o float são semelhantes, mas a diferença está na quantidade de bytes utilizados para armazenar valores.
+            Uma variável 'double' é capaz de armazenar números maiores.
 
-          ->> Por exemplo, uma variável do tipo 'double' seria capaz de armazenar o seguinte valor:
-          - 1.55505054050505054235053
-
-          * Mas uma variável do tipo 'float' não seria capaz. A parte decimal precisaria ser menor.
-
-          A questão é que, quando escrevemos o nosso valor quebrado, ele sempre
-          será considerado do tipo 'double'.
+            ->> Por exemplo, uma variável do tipo 'double' seria capaz de armazenar o seguinte valor:
+            - 1000000000000000000000000000000000000000.0
+            * Mas uma variável do tipo 'float' não seria capaz. Seria necessário diminuir aquele valor.
         */
 
+        // Mas a questão principal é que, ao escrevermos nosso valor quebrado, ele sempre será considerado do tipo 'double'.
         // EXEMPLOS:
 
-        // O valor contido entre parêntesis é do tipo 'double'.
+        // O valor contido entre parênteses é do tipo 'double'.
         System.out.println(500.0);
 
         // Aqui temos uma variável do tipo 'float' recebendo um valor quebrado:
         // >> float numQuebrado = 5.0;
 
         // O problema é que este '5.0' naturalmente é do tipo 'double'.
-        // Então, o que estamos tentando fazer, é armazenar um tipo 'double' numa variável 'float'.
+        // Então, o que estamos tentando fazer, é armazenar um valor do tipo 'double' numa variável 'float'.
+        // E o Java retorna o erro: "possible lossy conversion from double to float".
+        // O erro indica que pode haver perda na conversão de double para float.
 
-        // E o java retorna erro o seguinte erro: "possible lossy conversion from double to float".
-        // O erro diz que possivelmente pode haver uma perda na hora de converter o double para float.
+        // A realidade é que o Java não sabe o valor que temos, ele apenas reconhece que é 'double'.
+        // O double utiliza o dobro de bytes do 'float'. Portanto, estamos tentando armazenar muitos bytes onde não há espaço para eles.
 
-        // Pq a realidade é que o java não sabe qual o valor que temos, ele só sabe que aquele valor é 'double'.
-        // E o double, como já sabemos, utiliza o dobro de bytes do 'float'.
-        // Por consequência, estamos tentando armazenar muitos bytes onde não há espaço para tais.
-
-        // Para definir um valor FLOAT é necessário utilizar o 'F' no final do valor.
+        // Para definir um valor FLOAT é necessário utilizar 'F' no final do valor.
         float numQuebradoFloat = 5.0F;
+
+        // **** * * ** *** * ***** * *** ******* * * ********* * * * *
+
+        // INTEIROS: byte e int
+        /*
+            A mesma coisa vale para valores inteiros.
+            Quando um número é digitado, e se ele for menor que 128, ele será considerado por padrão, do tipo byte.
+
+            Caso o valor digitado seja 128 ou maior, ele será do tipo INT.
+        */
+        // Então mesmo que digatamos um numero enorme, que claramente seria 'long', ele será considerado do tipo 'integer'.
+        int 
+
     }
 }
