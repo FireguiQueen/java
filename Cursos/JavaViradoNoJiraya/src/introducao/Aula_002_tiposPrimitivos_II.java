4a41adb17f95cfa09169dcd10c610d27c53b8ffb
Guilherme <Fireguiquenbr@gmail.com>
2023-11-12 21:54:28 -0300

    Create Aula_002_tiposPrimitivos_II.java

---
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_002_tiposPrimitivos_II.java
@@ -0,0 +1,32 @@
+// TIPOS PRIMITIVOS: declaração e tamanho em memória
+/*
+    Cada tipo PRIMITIVO ocupa uma quantidade fixa de bits na memória RAM.
+    Isto é, o tipo 'int' utiliza 4bytes (32 bits) para armazenar números inteiros.
+
+    Isto é, um espaço em memória criado a partir de uma variável do tipo 'int', só consegue armazenar
+    valores entre -2.100.000.000 até +2.100.000.000
+    Isto ocorre justamente porque o int utiliza apenas 4bytes para armazenar números.
+    Se ultrapassarmos, por exemplo, do número '2.100.000.000' precisaremos de mais bits para representar esta quantia.
+
+    Para armazenar valores mais altos, utilize o tipo 'long'. O tipo 'long' utiliza 8bytes (64bits) para armazenar
+    valores.
+
+    Então, podemos penar que, quando criamos uma variável, por exemplo:
+    int x = 50;
+
+    A primeira coisa a ser feita é, criar um espaço em memória, onde sua capacidade de armzenato se baseia
+    no tipo de dado definido.
+    Como o tipo de dado definido na declaração foi 'int', então, é de se esperar que um espaço na memória RAM
+    seja criado com uma capacidade de armazenaot de 4bytes.
+
+    ** ** ** ** ** ** ** ** **
+
+*/
+
+package introducao;
+
+public class Aula_002_tiposPrimitivos_II {
+    public static void main(String[] args) {
+
+    }
+}
