8919333c081fff5aa4bfdae3ff2c56dedfe656d3
Guilherme <Fireguiquenbr@gmail.com>
2023-11-13 18:56:24 -0300

    Update Aula_003_tiposPrimitivos_III.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_003_tiposPrimitivos_III.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_003_tiposPrimitivos_III.java
@@ -1,4 +1,12 @@
+// CASTING  &  tipos-primitivos para decimais
+/*
+    O casting é uma conversão forçada entre valores numéricos.
+    Isto é, temos um valor de determinado tipo
+*/
 package introducao;
 
-public class Aula_003_tiposPrimitivos_III {
+public class Aula_003_tiposPrimitivos_III
+{
+    // DOUBLE VS FLOAT
+    // Quando t
 }
