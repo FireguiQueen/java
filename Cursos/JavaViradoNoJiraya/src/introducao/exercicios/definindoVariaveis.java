b4382087059f6ce77b0efe90462652e427fb3e4d
Guilherme <Fireguiquenbr@gmail.com>
2023-11-13 21:37:54 -0300

    Create definindoVariaveis.java

---
+++ Cursos/JavaViradoNoJiraya/src/introducao/exercicios/definindoVariaveis.java
@@ -0,0 +1,26 @@
+/*
+    1. DECLARE (SEM INICIALIZAR) UMA VARIÁVEL DO TIPO INT.
+    2. Inicialize ela com um valor inteiro.
+    --------------- -------------  ------------------------
+    1. Declare e inicialize uma variável do tipo 'long' com um valor acima de 3bi.
+    --------------- -------------  ------------------------
+    1. Declare e inicialize uma variável do tipo 'float'.
+    --------------- -------------  ------------------------
+    1. Declare e inicialize uma variável do tipo double.
+ */
+package introducao.exercicios;
+
+public class definindoVariaveis {
+    public static void main(String[] args)
+    {
+
+        int num;
+        num = 500;
+
+        long longNum = 500000000000000L;
+
+        float numQuebrado = 5.0F;
+
+        double numQuebradoMaior = 3.0;
+    }
+}
