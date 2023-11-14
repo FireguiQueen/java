2dce26660910f428ab29be51c57a0a526167a693
Guilherme <Fireguiquenbr@gmail.com>
2023-11-13 21:31:41 -0300

    Create Aula_004_Strings.java

---
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_004_Strings.java
@@ -0,0 +1,20 @@
+// STRINGS
+/*
+    Uma string é uma CADEI DE CARACTERES.
+    As strings são usadas para criar textos, palavras e etc.
+
+    Uma string NÃO é um tipo de dado primitivo.
+*/
+package introducao;
+
+public class Aula_004_Strings
+{
+    public static void main(String[] args)
+    {
+        // criando uma string
+        String nome = "Marcelo";
+
+        // printando a string
+        System.out.println(nome);
+    }
+}
