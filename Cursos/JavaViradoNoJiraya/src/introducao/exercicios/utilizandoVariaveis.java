e2aa1b4c05db7356d59adf06eced01186560ad57
Guilherme <Fireguiquenbr@gmail.com>
2023-11-13 22:11:05 -0300

    Update utilizandoVariaveis.java

--- Cursos/JavaViradoNoJiraya/src/introducao/exercicios/utilizandoVariaveis.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/exercicios/utilizandoVariaveis.java
@@ -13,13 +13,13 @@ public class utilizandoVariaveis {
 
         int cep = 23289320;
         String address = "Oswvaldo Weldir";
-        String data = "12-03-2024";
+        String dataRecebimento = "12-03-2024";
 
         // printando
-        System.out.println("Eu, " + name + ", morando no endereço " + (address +' '+cep) + ", confirmo que recebi o salário de " + salario + " na data de " + data);
+        System.out.println("Eu, " + name + ", morando no endereço " + (address +' '+cep) + ", confirmo que recebi o salário de " + salario + " na data de " + dataRecebimento);
 
         // outro jeito de printar (mais prático)
-        System.out.format("Eu, %s, morando no endereço %s %d, confirmo que recebi o salário de %.2f, na data de %s \n", name, address, cep, salario, data);
+        System.out.format("Eu, %s, morando no endereço %s %d, confirmo que recebi o salário de %.2f, na data de %s \n", name, address, cep, salario, dataRecebimento);
 
 
         // Para facilitar a questão do endereço, podemos criar uma variável juntando o CEP + ADDRESS.
