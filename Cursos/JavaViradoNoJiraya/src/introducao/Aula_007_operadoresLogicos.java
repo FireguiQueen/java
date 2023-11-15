4b961899398b6b6795edf195e0ee9700de17a21e
Guilherme <Fireguiquenbr@gmail.com>
2023-11-14 21:44:23 -0300

    Update Aula_007_operadoresLogicos.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_007_operadoresLogicos.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_007_operadoresLogicos.java
@@ -16,39 +16,43 @@ public class Aula_007_operadoresLogicos
 {
     public static void main(String[] args)
     {
-        // Abaixo, podemos ver o que seria uma expressão "normal".
-        int num = (50 + 50) * 2;
-        // Nossa expressão retorna o valor de 200.
+        // Abaixo, temos uma expressão "normal".
+        int num = (50 + 50) * 2; // Essa expressão retorna o valor de 200.
 
-        // Aqui, podemos ver uma expressão que utiliza um operador de COMPARAÇÃO.
-        // Por estamos usando este operador, ela deve retornar 'TRUE' ou 'FALSE'.
+        // Agora, uma expressão que utiliza um operador de COMPARAÇÃO, retornando 'true' ou 'false'.
         boolean y = 20 > 10;
 
-        // Mas como poderíamos colocar os operadores lógicos em expressões?
-        // É bem simples, observe o exemplo abaixo:
-        /*
-            Estamos construindo um sistema onde avalia a condição financeira da pessoa como "ruim" ou "boa".
-            Nosso sistema é bem idiota, mas serve como exemplo.
-            Para você ter uma condição financeira boa, você deve atingir os seguintes requisitos, isto é
-            AMBAS as perguntas precisam ser respondidas com 'true' para você estar bom financeiramente:
-            - Tem uma casa?
-            - Tem mais de 30.000 no banco?
-        */
+        // Vamos usar operadores lógicos em expressões.
+        // Neste exemplo, avaliamos a condição financeira considerando dois requisitos:
+        // 1. Ter uma casa.
+        // 2. Ter mais de 30.000 no banco.
+
         boolean temCasa = true;
         boolean temTrintaMil = true;
-        boolean condicaoFinanceiraBoa = temCasa == true  &&  temTrintaMil == true;
+        boolean condicaoFinanceiraBoa = temCasa && temTrintaMil;
 
-        // EXPRESSÃO COMPLETA. Ela é responsável por dizer 'true' ou 'false':
-        //  temCasa == true  &&  temTrintaMil == true
+        // Expressão completa: temCasa && temTrintaMil
+        // Aqui, o operador lógico AND (&&) verifica se ambas as condições são 'true'.
+        // Se sim, a expressão completa também é 'true'.
 
-        // EXPRESSÕES DENTRO DA NOSSA EXPRESSÃO. Elas são responsáveis por perguntar se as respostas foram respondidas com 'true':
-        //  temCasa == true
-        //  temTrintaMil == true
+        // LEMBRE-SE, quando utilizad o operador 'AND', para que a expressão completa retorne 'true', as expressões precisam ser verdadeiras.
 
-        // Primeiro é perguntado se a variável 'temCasa' é igual a 'TRUE'.
-        // Depois é perguntado se a variável 'temTrintaMil' é igual a 'TRUE'.
-        // E entre essas duas expressões, temos o operador lógico AND.
-        // Ele será responsável por capturar as duas respostas, e com base nelas, fazer com que a expressão retorna true ou false.
-        // Como as duas são 'true', o and fará com que a expressão retorne 'true'.
+        // EXEMPLOS:
+        System.out.println(true && true); // true (true pois ambas são verdadeiras)
+        System.out.println(true && false); // false (ambas deveriam ser verdadeiras)
+        System.out.println(true && true && true); // true (todas são verdadeiras)
+        System.out.println(false && true && true); // false (pois nem todas são true).
+
+        // Verificar se poder beber bebidas alcoolicas: precisa ser de maior e ter 5 reais na carteira.
+        double carteira = 30.0;
+        int idade = 20;
+        boolean podeComprarABreja = carteira >= 5 && idade >= 18;
+        // a variável 'podeComprarABreja' irá receber 'TRUE'. Pois a carteira é maior que 5 e a idade é maior que 18.
+
+
+        // ****** * * ********* * ************ *** ****** ****** * ************ * * *************
+
+        // Operador 'or' (||)
+
     }
 }
