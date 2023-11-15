0acd3a63248395ac18b0f0b4448aed6a108ab4ba
Guilherme <Fireguiquenbr@gmail.com>
2023-11-15 09:50:33 -0300

    Update Aula_008___operadoresDeAtribuicao.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_008___operadoresDeAtribuicao.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_008___operadoresDeAtribuicao.java
@@ -1,6 +1,7 @@
 // OPERADORES DE ATRIBUIÇÃO - Syntactic sugar
 /*
-    "Syntatic sugar" é, na verdade, alguns jeitos mais "bonitos" e "simples" de se realizar alguns instruções
+    "Syntatic sugar" é uma forma sintaxe que é designada para fazer as coisas serem mais fáceis de serem
+    lidas ou expressadas. Faz a linguagem "mais doce" para o uso humano.
 
     ** São eles:
     valor da variavel mais outro valor: +=
@@ -23,15 +24,23 @@ public class Aula_008___operadoresDeAtribuicao
             Você tem uma conta no banco com R$10.000;
             - Você deseja comprar Y produto.
             Ao realizar a compra, é de se esperar que o saldo da sua conta seja diminuido de acordo com o preço do produto.
-        */ // Em código, ficaria assim:
+        */ // Em código, podemos fazer isso de duas maneiras:
+
+        // 1. Jeito "padrão":
         double saldoContaBancaria = 10000;
         double precoDoProduto = 300;
-
-        // Ao realizar a compra:
         saldoContaBancaria = saldoContaBancaria - precoDoProduto;
-        // Isto é: quero que a variavel "saldo da conta" receba o valor dela (10.000) menos o precoDoProduto (300);
+        // Isto é: quero que a variavel "saldoConta.." receba o valor dela (10.000) menos o precoDoProduto (300);
+
+        // 2. Syntatic sugar:
+        double saldoContaBancaria0 = 10000;
+        double precoProduto = 300;
+        saldoContaBancaria0 -= precoProduto;
+        // isto é: a variável "saldoContaBancaria" terá seu valor como ela mesma MENOS (-) a variável preço produto.
 
 
+
+        
 
     }
 }
