f0e49cf6b9058af34b95c56374a501ba97e7fe5d
Guilherme <Fireguiquenbr@gmail.com>
2023-11-15 10:40:26 -0300

    Update Aula_008___operadoresDeAtribuicao.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_008___operadoresDeAtribuicao.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_008___operadoresDeAtribuicao.java
@@ -1,7 +1,7 @@
 // OPERADORES DE ATRIBUIÇÃO - Syntactic sugar
 /*
-    "Syntatic sugar" é uma forma sintaxe que é designada para fazer as coisas serem mais fáceis de serem
-    lidas ou expressadas. Faz a linguagem "mais doce" para o uso humano.
+    "Syntactic sugar" é uma forma de sintaxe projetada para tornar as coisas mais fáceis de serem
+    lidas ou expressadas. Torna a linguagem "mais doce" para o uso humano.
 
     ** São eles:
     valor da variavel mais outro valor: +=
@@ -9,38 +9,51 @@
     valor da variavel vezes outro valor: *=
     valro da variável dividido por outro valor: /=
 
-    A ideia deles é que você sempre utilize o valor da variável
-    juntamente a um operador
+    A ideia por trás deles é que você possa usar o valor da variável
+    juntamente com um operador para realizar a operação desejada.
 */
 
 package introducao;
-
 public class Aula_008___operadoresDeAtribuicao
 {
     public static void main(String[] args)
     {
         // EXEMPLOS
         /*
-            Você tem uma conta no banco com R$10.000;
-            - Você deseja comprar Y produto.
+            -> Você tem uma conta no banco com R$10.000;
+            ->  Você deseja comprar Y produto.
             Ao realizar a compra, é de se esperar que o saldo da sua conta seja diminuido de acordo com o preço do produto.
-        */ // Em código, podemos fazer isso de duas maneiras:
+        */
 
         // 1. Jeito "padrão":
         double saldoContaBancaria = 10000;
         double precoDoProduto = 300;
         saldoContaBancaria = saldoContaBancaria - precoDoProduto;
-        // Isto é: quero que a variavel "saldoConta.." receba o valor dela (10.000) menos o precoDoProduto (300);
+        // Isto é: quero que a variavel "saldoContaBancaria" receba o valor dela (10.000) menos o 'precoDoProduto' (300);
 
         // 2. Syntatic sugar:
         double saldoContaBancaria0 = 10000;
         double precoProduto = 300;
         saldoContaBancaria0 -= precoProduto;
-        // isto é: a variável "saldoContaBancaria" terá seu valor como ela mesma MENOS (-) a variável preço produto.
+        // isto é: a variável "saldoContaBancaria" receberá ela mesma MENOS (-) a variável 'precoProduto'.
+
+
+        // Outros exemplos:
+        int num = 10;
+        num *= 2;  // seria a mesma coisa que fazer: num = num * 2;
+
+        // VALE LEMBRAR QUE, A VARIAVEL JA DEVE POSSUIR UM VALOR. FAZER ISSO RETORNARÁ ERRO.
+        // int numeroInteiro;
+        // numeroInteiro = numeroInteiro * 2;
+        // >>>>      ou
+        // int numeroInteiro;
+        // numeroInteiro *= 2;
 
 
 
-        
+
+
+
 
     }
 }
