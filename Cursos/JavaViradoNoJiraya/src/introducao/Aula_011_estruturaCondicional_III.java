956c95b5c60da2220836a093364037741a0f6fc0
Guilherme <Fireguiquenbr@gmail.com>
2023-11-16 16:15:46 -0300

    Update Aula_011_estruturaCondicional_III.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_011_estruturaCondicional_III.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_011_estruturaCondicional_III.java
@@ -10,29 +10,40 @@ public class Aula_011_estruturaCondicional_III
 {
     public static void main(String[] args) {
         // Começaremos com uma expressão e uma interrogação.
-        // Caso ela seja avaliada como "true", a primeira parte é executada, caso ela seja false, a segunda parte será.
+        // Caso ela seja avaliada como "true", a primeira parte é retornada, caso seja false, a segunda parte será.
 
         double contaBancaria = 8000;
-        String resultado = contaBancaria >= 3000 ? "Vou comprar o PS5" : "Não vou comprar o PS5";
+        String resultado = (contaBancaria >= 3000) ? "Vou comprar o PS5" : "Não vou comprar o PS5";
         System.out.println(resultado);
 
-        // Primeiro foi definido uma variável do tipo double, e ela representa nosso saldo.
-        // Depois é temos a expressão "contaBancaria >= 3000".
-        // >> Se for 'true', será retornado "Vou comprar o PS5"
-        // >> Se for 'false' será retornado "Não vou comprar o PS5"
+        // Primeiro, foi definida uma variável do tipo double, representando o nosso saldo do banco.
+        // Depois, temos a expressão "contaBancaria >= 3000".
+        // >> Se for 'true', será retornado "Vou comprar o PS5" a variável 'resultado'.
+        // >> Se for 'false', será retornado "Não vou comprar o PS5" a variável 'resultado'.
 
-        // A nossa expressão é TRUE, sendo assim, é retornado "vou comprar o PS5" para a variável 'resultado';
+        // ** A nossa expressão é TRUE, assim, é retornado "Vou comprar o PS5" para a variável 'resultado'.
 
         /*
             Vale lembrar que este operador RETORNA algum valor, e por isso, ele deve ser colocado como o valor de uma variável
             ou passado como argumento para um método.
 
-            Então NÃO podemos simplesmente taca-lo em nosso código assim:
-            > contaBancaria >= 3000? "Vou comprar o PS5" : "Não vou comprar o PS5";
+            Então NÃO podemos simplesmente tacá-lo em nosso código assim:
+            > contaBancaria >= 3000 ? "Vou comprar o PS5" : "Não vou comprar o PS5";
 
             Como a expressão é verdadeira, pois contaBancaria (8000) é maior que 3000, seria a mesma
             coisa que colocar a string no código dessa forma:
             > "Vou comprar o PS5";
+
+
+            Ele deve estar assim:
+
+            // dando o retorno para uma variável:
+            resultado = contaBancaria >= 3000 ? "Vou comprar o PS5" : "Não vou comprar o PS5";
+
+            ou assim
+
+            // dando o retorno para um método:
+            System.out.println(contaBancaria >= 3000 ? "Vou comprar o PS5" : "Não vou comprar o PS5";)
         */
     }
 }
