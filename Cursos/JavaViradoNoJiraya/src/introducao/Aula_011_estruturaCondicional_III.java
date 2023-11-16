0b1d43a8eab2253c2ed2c8c60dfa2cd309e999b8
Guilherme <Fireguiquenbr@gmail.com>
2023-11-16 16:12:48 -0300

    Update Aula_011_estruturaCondicional_III.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_011_estruturaCondicional_III.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_011_estruturaCondicional_III.java
@@ -2,36 +2,37 @@
 /*
     O operador ternário é utilizado para substituir uma estrutura condicional simples,
     que envolva apenas um "if" e um "else".
-
-
 */
 package introducao;
 
 
 public class Aula_011_estruturaCondicional_III
 {
-    public static String oi()
-    {
-        System.out.println("oi");
-        return;
-    }
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         // Começaremos com uma expressão e uma interrogação.
         // Caso ela seja avaliada como "true", a primeira parte é executada, caso ela seja false, a segunda parte será.
 
         double contaBancaria = 8000;
-        String vouSairHoje = contaBancaria >= 3000? "Vou comprar o PS5" : "Não vou comprar o PS5";
+        String resultado = contaBancaria >= 3000 ? "Vou comprar o PS5" : "Não vou comprar o PS5";
+        System.out.println(resultado);
 
         // Primeiro foi definido uma variável do tipo double, e ela representa nosso saldo.
         // Depois é temos a expressão "contaBancaria >= 3000".
         // >> Se for 'true', será retornado "Vou comprar o PS5"
-        // >> Se for 'false' seŕa retornado "Não vou comprar o PS5"
+        // >> Se for 'false' será retornado "Não vou comprar o PS5"
 
-        System.out.println(5==5? oi() : "cu");
+        // A nossa expressão é TRUE, sendo assim, é retornado "vou comprar o PS5" para a variável 'resultado';
+
         /*
-            Vale lembrar que este operador RETORNA
+            Vale lembrar que este operador RETORNA algum valor, e por isso, ele deve ser colocado como o valor de uma variável
+            ou passado como argumento para um método.
+
+            Então NÃO podemos simplesmente taca-lo em nosso código assim:
+            > contaBancaria >= 3000? "Vou comprar o PS5" : "Não vou comprar o PS5";
+
+            Como a expressão é verdadeira, pois contaBancaria (8000) é maior que 3000, seria a mesma
+            coisa que colocar a string no código dessa forma:
+            > "Vou comprar o PS5";
         */
     }
 }
