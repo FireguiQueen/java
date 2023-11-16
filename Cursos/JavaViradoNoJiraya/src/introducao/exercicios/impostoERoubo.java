17d4a3e2b6ef5dd5a34a2498f0951a6d325e74ce
Guilherme <Fireguiquenbr@gmail.com>
2023-11-16 16:49:20 -0300

    Update impostoERoubo.java

--- Cursos/JavaViradoNoJiraya/src/introducao/exercicios/impostoERoubo.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/exercicios/impostoERoubo.java
@@ -2,6 +2,21 @@ package introducao.exercicios;
 
 public class impostoERoubo
 {
-    double salario = 5000;
-    
+    public static void main(String[] args)
+    {
+        double salary = 5000;
+        double totalTaxes;
+
+        if (salary >= 0 && salary <= 34712)
+        {
+            totalTaxes = 9.70;
+            salary *= totalTaxes / 100;
+        } else if (salary >= 34713 && salario <= 68507)
+        {
+            totalTaxes = 37.35;
+            salary *= totalTaxes / 100;
+        }
+
+        System.out.println("Valor liq.: " + salario + ". Total texes: " + taxe);
+    }
 }
