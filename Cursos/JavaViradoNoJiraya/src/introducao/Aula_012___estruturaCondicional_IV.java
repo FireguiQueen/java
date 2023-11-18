93ef90e07e03b24965d3abeea42faa972c581068
Guilherme <Fireguiquenbr@gmail.com>
2023-11-18 16:47:00 -0300

    Update Aula_012___estruturaCondicional_IV.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_012___estruturaCondicional_IV.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_012___estruturaCondicional_IV.java
@@ -1,16 +1,15 @@
+// ESTRUTURA CONDICIONAL: SWITCH CASE
+/*
+    O switch case é utilizado como se fosse um 'if/else..' porém
+    apenas para condições de igu
+*/
 package introducao;
 
 public class Aula_012___estruturaCondicional_IV
 {
     public static void main(String[] args)
     {
-        double oi = 2;
-        switch(55)
-        {
-            case '7':
-                System.out.println("oi");
-                break;
-        }
+
 
     }
 }
