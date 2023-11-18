6f8fdc51f5f0e4fbb4e16fa0e1ba613cb422674a
Guilherme <Fireguiquenbr@gmail.com>
2023-11-18 10:16:08 -0300

    Create loginSimples.java

---
+++ Cursos/JavaViradoNoJiraya/src/introducao/exercicios/loginSimples.java
@@ -0,0 +1,31 @@
+/*
+Login Simples:
+    Implemente um programa de login simples. Peça ao usuário que insira um nome de usuário e uma senha. Se o nome de
+    usuário for "admin" e a senha for "senha123", exiba uma mensagem de boas-vindas, caso contrário, exiba uma mensagem de erro.
+*/
+package introducao.exercicios;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class loginSimples
+{
+    public static void main(String[] args)
+    {
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Usuário: ");
+        String user = userInput.next();
+
+        System.out.print("Senha: ");
+        String password = userInput.next();
+
+
+        System.out.println("Codigo de acesso (opcional): ");
+        long codeAcess = userInput.nextLong();
+
+        String checkUserCredentials = (Objects.equals(user, "admin") && Objects.equals(password, "senha123") || codeAcess == 675734634634634634L)? "Olá, seja bem vindo!" : "Usuário ou senha incorretos.";
+
+        System.out.println(checkUserCredentials);
+
+    }
+}
