3fd2b7af1cf2a00c0dad33a6219abd2d5e5b00a2
Guilherme <Fireguiquenbr@gmail.com>
2023-11-18 17:05:43 -0300

    Update Aula_012___estruturaCondicional_IV.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_012___estruturaCondicional_IV.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_012___estruturaCondicional_IV.java
@@ -1,12 +1,13 @@
 // ESTRUTURA CONDICIONAL: SWITCH CASE
 /*
-    O switch case é utilizado como se fosse um 'if/else..' porém
-    apenas para condições de igualdade.
+    O switch case é utilizado como se fosse um 'if/else..', porém, apenas para condições de igualdade.
 
-    Ao lado da palavra-chave "switch", colocaremos parênteses, e dentro deles, colocaremos o valor
-    que desejamos verificar pelos casos.
+    Ao lado da palavra-chave "switch", colocaremos parênteses, e dentro deles, colocaremos algum valor, sendo ele:
+    byte, short, int, long, char ou String
 
-
+    Dentro da estrutura, usaremos a palavra-chave "case".
+    Casso haja correspondencia entre algum caso em relação ao valor que foi passado para o switch, as instruções
+    associadas a esse caso serão executadas.
 */
 package introducao;
 
@@ -14,7 +15,9 @@ public class Aula_012___estruturaCondicional_IV
 {
     public static void main(String[] args)
     {
-        
-
-    }
+        switch (10)
+        {
+            case 0:
+                System.out.println("");
+        }
 }
