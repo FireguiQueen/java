c62f1873912360dd5624b084d56151c07085d8d8
Guilherme <Fireguiquenbr@gmail.com>
2023-11-19 11:07:46 -0300

    update

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_016___estruturasDeRepeticao_III.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_016___estruturasDeRepeticao_III.java
@@ -1,11 +1,12 @@
 // ESTRUTURAS DE REPETIÇÃO: BREAK
 /*
-    A palavra-chave 'break' significa "encerrar", 'quebrar', "interromper". O 'break' é muito utilizado em
-    loops para quebrá-los, isto é, quando estamos em uma repetição, ao chegar na instrução 'break', o loop
-    é encerrado.
+    A palavra-chave 'break' significa "encerrar", 'quebrar', "interromper".
+    O 'break' é frequentemente utilizado em loops para interrompê-los. Em outras palavras,
+    quando encontramos a instrução 'break' dentro de um loop, o ciclo é encerrado imediatamente.
 
-    A utilidade chega quando usamos estruturas condicionais, colocando determinada condição, e caso ela seja true,
-    o 'break' será executado, interrompendo as proximas repetições.
+    A utilidade do 'break' torna-se evidente ao combiná-lo com estruturas condicionais.
+    Ao estabelecer uma condição específica, caso seja avaliada como verdadeira,
+    a execução do 'break' ocorre, interrompendo as próximas iterações do loop.
 */
 package introducao;
 
@@ -14,38 +15,34 @@ public class Aula_016___estruturasDeRepeticao_III
     public static void main(String[] args)
     {
 
-        // Aqui temos um loop que vai ser executado 51 vezes. (de 0 até 50)
+        // Aqui temos um loop que será executado 51 vezes (de 0 até 50).
         for (int i = 0; i <= 50; i++)
         {
-            if (i > 25) // se 'i' for maior que 25 o bloco 'if' será executado.
+            if (i > 25) // Se 'i' for maior que 25, o bloco 'if' será executado.
             {
-                break;
+                break; // caso esse 'break' seja executado, o loop é quebrado.
             }
             System.out.println("O valor de i é: " + i);
         }
 
         // EXPLICANDO
         /*
-            No primeiro ciclo por exemplo, i será 0.
-            Será perguntado: 0 <= 50? TRUE (pois 0 é menor que 50)
-            O bloco 'for' será executado.
-            --> Dentro deste bloco, temos um 'if', e ele pergunta se a variável i (0) é maior que 25
+            No primeiro ciclo a variável 'i' será 0.
+            Será verificado: 0 <= 50, o que é VERDADEIRO. O bloco 'for' será executado.
+            --> Dentro deste bloco, temos um 'if', que pergunta se a variável i (0) é maior que 25.
             ----- Isto é falso, pois i (0) não é maior que 25.
 
-            E isso vai até 'i' valer 25.
-            Será perguntado: 25 <= 50? TRUE (pois 25 é menor que 50)
+            Isso continuará até 'i' valer 25.
+            Será verificado se 25 <= 50, o que é VERDADEIRO.
             - O bloco 'for' será executado.
-            --> Dentro deste bloco, temos um 'if', e ele pergunta se a variável i (25) é maior que 25
-            ----- Isto é falso, pois i (25) não é maior que 25.
+            --> Dentro deste bloco, temos um 'if' que pergunta se a variável i (25) é maior que 25.
+            ----- Isso é falso, pois i (25) não é maior que 25.
 
-            Agora 'i' vale 26.
-            Será perguntado: 26 <= 50? TRUE
-            - O bloco 'for' será executado.
-            --> Dentro deste bloco, temos um 'if', e ele pergunta se a variável i (26) é maior que 25
-            ----- Isto é VERDADEIRO, pois i (26) é maior que 25.
+            Agora, 'i' vale 26.
+            Será verificado se 26 <= 50, o que é VERDADEIRO. O bloco 'for' será executado.
+            --> Dentro deste bloco, temos um 'if' que pergunta se a variável i (26) é maior que 25.
+            ----- Isso é VERDADEIRO, pois i (26) é maior que 25.
             ------ A instrução 'break' é executada, e o loop é finalizado.
         */
     }
-
-
 }
