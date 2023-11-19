b57d73ec8a7c4bfd3389d4f20f34cf429e630235
Guilherme <Fireguiquenbr@gmail.com>
2023-11-19 18:07:59 -0300

    Update Aula_018___estruturasDeRepeticao_V.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_018___estruturasDeRepeticao_V.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_018___estruturasDeRepeticao_V.java
@@ -1,39 +1,47 @@
 // ESTRUTURAS DE REPETIÇÃO: CONTINUE
 /*
-    "continua" significa "continuar.
-    Quando é encontrado o 'break' o loop se encerra, não executa mais.
-    Já o 'continue' fez com que o loop vá para a próxima iteração.
+    A palavra-chave 'continue' é utilizada para pular para a próxima iteração
+    de um loop, ignorando as instruções restantes dentro do bloco do loop.
+
+    Quando o 'continue' é encontrado, o loop não é encerrado; em vez disso,
+    ele passa para a próxima iteração, ignorando as instruções subsequentes.
+
+    Vamos analisar alguns exemplos para entender melhor como o 'continue' funciona.
 */
+
 package introducao;
 
-public class Aula_018___estruturasDeRepeticao_V
-{
+public class Aula_018___estruturasDeRepeticao_V {
     public static void main(String[] args) {
 
-        // Irá ser executado 10 vezes.
-        for (int i = 0; i < 10; i++)
-        {
-
-            // caso 'i' seja menor ou igual a 5, o for irá ir para o próximo ciclo de execução.
-            if (i <= 5){
-                continue;
+        // Exemplo 1: Utilizando 'continue' para pular iterações em um intervalo específico.
+        for (int i = 0; i < 10; i++) {
+            if (i <= 5) {
+                continue; // Pula para a próxima iteração se 'i' for menor ou igual a 5.
             }
             System.out.println("O valor de i é: " + i);
         }
 
-        // EXPLICANDO
         /*
-            Na primeira vez, o 'i' vale 0.
-            Será perguntado: 0 < 10? (true).
-            O bloco é executado.
-            -> dentro do bloco há uma condição perguntando: i (0) <= 5? (true)
-            ---> dentro deste bloco 'if', há uma instrução de 'continue', ou seja, o for será executado novamente.
-
-            E este 'continue' só vai parar quando chegar no 6.
-            -> dentro do bloco há uma condição perguntando: i (6) <= 5? (false)
-            >> Como o 'continue' não foi executado, agora, as próximas instruções serão lidas. e o valor de 'i' será printado.
+            No exemplo acima, o 'for' é executado 10 vezes. Se 'i' for menor ou igual a 5,
+            a instrução 'continue' faz com que o loop vá diretamente para a próxima iteração,
+            ignorando o restante do bloco de código dentro do loop. A impressão só ocorre
+            quando 'i' é maior que 5.
         */
 
+        // Exemplo 2: Utilizando 'continue' para ignorar números pares.
+        int range = 50;
+        for (int i = 0; i <= range; i++) {
+            if (i % 2 == 0) {
+                continue; // Pula para a próxima iteração se 'i' for par.
+            }
+            System.out.println("É ímpar: " + i);
+        }
 
+        /*
+            Neste exemplo, o 'continue' é usado para fazer o loop ignorar números pares.
+            Se o resto da divisão de 'i' por 2 for igual a 0, indica que 'i' é par, e
+            o 'continue' faz o loop pular para a próxima iteração sem imprimir números pares.
+        */
     }
 }
