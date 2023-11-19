661ee3f9ea282be96b7b480ffd4fe8c16442d172
Guilherme <Fireguiquenbr@gmail.com>
2023-11-19 11:30:04 -0300

    Update Aula_017___estruturasDeRepeticao_IV.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_017___estruturasDeRepeticao_IV.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_017___estruturasDeRepeticao_IV.java
@@ -1,7 +1,25 @@
+// ESTRUTURAS DE REPETIÇÃO: BREAK
+// --> EXERCÍCIO <--
 package introducao;
 
 public class Aula_017___estruturasDeRepeticao_IV
 {
+    public static void main(String[] args)
+    {
+        double valorDoProduto = 10000;
 
-    djisdgdigh
+        for (int i = 1; i <= valorDoProduto; i++)
+        {
+            double valorParcela = valorDoProduto / i;
+            if (valorParcela < 1000)
+            {
+                break;
+            }
+            System.out.println("Parcela " + i + ": " + valorParcela);
+
+        }
+
+    }
+
+
 }
