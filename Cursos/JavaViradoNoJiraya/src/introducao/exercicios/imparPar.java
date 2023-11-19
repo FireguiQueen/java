b05ce22dba07668461f012e8277b7c81a7f86914
Guilherme <Fireguiquenbr@gmail.com>
2023-11-19 19:02:10 -0300

    Update imparPar.java

---
+++ Cursos/JavaViradoNoJiraya/src/introducao/exercicios/imparPar.java
@@ -0,0 +1,39 @@
+package introducao.exercicios;
+import java.util.Scanner;
+
+
+public class imparPar
+{
+    public static void main(String[] args) {
+        Scanner userIn = new Scanner(System.in);
+        int range;
+
+        System.out.print("Insira um valor inteiro positivo: ");
+        do
+        {
+            range = userIn.nextInt();
+            System.out.println(range <= 0? "Insira um valor válido." : "");
+        } while (range <= 0);
+
+        // algoritimo ruim. criado apenas devido ao exercicio (o continue é desnecessário)
+        for (int i = 0; i <= range; i++)
+        {
+            System.out.print("\nO número " + i + " é: ");
+            if (i % 2 == 0 ) {
+                System.out.print("par");
+                continue;
+            }
+            System.out.print("impar");
+        }
+
+        // OU
+
+        for (int i = 0; i <= range; i++)
+        {
+            String isOddOrEven = i % 2 == 0? "par" : "ímpar";
+            System.out.println(i + " é: " + isOddOrEven);
+        }
+
+    }
+
+}
--- Cursos/JavaViradoNoJiraya/src/introducao/exercicios/numerosPrimos.java
+++
@@ -1,4 +0,0 @@
-package introducao.exercicios;
-
-public class numerosPrimos {
-}
