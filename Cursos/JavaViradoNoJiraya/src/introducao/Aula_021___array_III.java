bb47a61f24a67a4ac5f50357617fcb1900922816
Guilherme <Fireguiquenbr@gmail.com>
2023-11-19 23:13:33 -0300

    Update Aula_021___array_III.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_021___array_III.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_021___array_III.java
@@ -1,5 +1,20 @@
-// ARRAY: iterando (percorrendo) 
+// ARRAY: iterando (percorrendo)
+/*
+    "Iterar um array" é a mesma coisa que "percorrer um array", isto é, acessar cada um dos elementos do array.
+    Para iterar, podemos usar o for loop.
+*/
 package introducao;
 
-public class Aula_021___array_III {
+public class Aula_021___array_III
+{
+    public static void main(String[] args)
+    {
+        // define um array de números inteiros. O array pode conter no máximo 2 elementos.
+        int[] numerosInteiros = new int[3];
+
+        // 'length' é a propriedade do objeto 'array'. Ele retorna a quantidade de elementos que o array possui.
+        int 
+
+        for (int i = 0; i < )
+    }
 }
