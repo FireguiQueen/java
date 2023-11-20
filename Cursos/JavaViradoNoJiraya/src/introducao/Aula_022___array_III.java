c5ea335f8d1520f823b7b0accf625c53d978a5d3
Guilherme <Fireguiquenbr@gmail.com>
2023-11-20 16:53:43 -0300

    Create Aula_022___array_III.java

---
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_022___array_III.java
@@ -0,0 +1,39 @@
+// ARRAY: iterando mais facilmente
+/*
+    Para iterar de uma maneira simplificada um array, podemos usar o conceito "for each".
+    Teremos uma variável, e a cada ciclo, será atribuido um elemento do array para ela.
+*/
+
+package introducao;
+
+public class Aula_022___array_III
+{
+    public static void main(String[] args)
+    {
+        int[] numerosInteiros = {100, 200, 300, 400};
+
+        for (int num : numerosInteiros)
+        {
+            System.out.println(num);
+        }
+
+        // EXPLICANDO
+        /*
+            Na primeira vez:
+            > 'num' recebe numerosInteiros[0]
+            > System.out.println(num); // 100
+
+            Na segunda vez:
+            > 'num' recebe numerosInteiros[1]
+            > System.out.println(num); // 200
+
+            > 'num' recebe numerosInteiros[2]
+            > System.out.println(num); // 300
+
+            > 'num' recebe numerosInteiros[3]
+            > System.out.println(num); // 400 
+        */
+
+    }
+}
+
