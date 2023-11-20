c93eb06a7e640cc865062fc319c02dac3fc0336e
Guilherme <Fireguiquenbr@gmail.com>
2023-11-19 23:28:30 -0300

    Update Aula_021___array_III.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_021___array_III.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_021___array_III.java
@@ -12,9 +12,64 @@ public class Aula_021___array_III
         // define um array de números inteiros. O array pode conter no máximo 2 elementos.
         int[] numerosInteiros = new int[3];
 
-        // 'length' é a propriedade do objeto 'array'. Ele retorna a quantidade de elementos que o array possui.
-        int 
+        /*
+            Também poderia ser feito assim:
+            > int[] numerosInteiros;
+            > numerosInteiros = new int[3];
+        */
 
-        for (int i = 0; i < )
+        // definindo valores para o nosso array:
+        numerosInteiros[0] = 100;
+        numerosInteiros[1] = 500;
+        numerosInteiros[2] = 1000;
+
+        // 'length' é a propriedade do objeto 'array'. Ele retorna a quantidade de elementos que o array possui.
+        int quantidadeElementos = numerosInteiros.length; // retorna 3
+
+        // irá printar todos os elementos do array: 100, 500, 1000
+        for (int i = 0; i < quantidadeElementos; i++)
+        {
+            System.out.println(numerosInteiros[i]);
+        }
+
+        // EXPLICANDO
+        /*
+            A condição precisa ser "MENOR" pois para acessar um valor de um array, usamos seu indice.
+            Então por exemplo, se o array possui 3 elementos, ele possui 2 indices: 0, 1 e 2.
+
+            Primeiro é definido uma variável 'i' e seu valor é '0'.
+            > Temos a condição: 0 < quantidadeElementos (3)?
+            A condição é verdadeira, então a instrução é executada:
+            > System.out.println(numerosInteiros[0]); // printa o valor da posição 0. (primeiro elemento do array)
+            > i++
+
+            Agora 'i' vale '1'
+            > condição: 1 < quantidadeElementos (3)?
+            A condição é verdadeira, então a instrução é executada:
+            > System.out.println(numerosInteiros[1]); // printa o valor da posição 1. (segundo elemento do array)
+            > i++
+
+            Agora 'i' vale '2'
+            > condição: 2 < quantidadeElementos (3)?
+            A condição é verdadeira, então a instrução é executada:
+            > System.out.println(numerosInteiros[2]); // printa o valor da posição 2. (terceiro elemento do array)
+            > i++
+
+            Agora 'i' vale '3'
+            > condição: 3 < quantidadeElementos (3)?
+            A condição é falsa. O loop é encerrado.
+
+
+            *** SE A GENTE COLOCASSE 'MAIOR IGUAL': i <= quantidadeElementos
+            Quando 'i' valesse '3', a condição seria:
+            3 <= quantidadeElementos (3)?
+            Verdade, pois mesmo que três não seja menor que três.. três é igual a três.
+            O bloco seria executado.
+            > System.out.println(numerosInteiros[3]); // printa o valor da posição 3 (mas ele não existe. Retorna erro.)
+
+
+
+        */
+
     }
 }
