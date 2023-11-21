b6918e2980f6413b821de8e4682173bbb37dd0fb
Guilherme <Fireguiquenbr@gmail.com>
2023-11-21 10:10:18 -0300

    Update Aula_025___array_VI.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_025___array_VI.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_025___array_VI.java
@@ -9,8 +9,11 @@ public class Aula_025___array_VI
 
         // Aqui estamos criando um array que pode receber outros arrays.
         // Mas como não passamos a quantidade de elementos que os arrays secundarios irão conter, eles terão o valor 'null'
-        int[][] arrVazio = new int[3][];
+        int[][] arrVazio = new int[2][];
 
-        
+        // Agora podemos criar referencias a os arrays do array principal
+        arrVazio[0] = new int[2]; // posição 0 do array principal ira fazer referencia a um array de 2 elementos
+        arrVazio[1] = new int[5]; // posição 1 do array principal ira fazer referencia a um array de 5 elementos
+
     }
 }
