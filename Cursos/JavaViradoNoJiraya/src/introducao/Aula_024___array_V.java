b5ad8b88989c13ed43ac43f7c69dfe2547754f2f
Guilherme <Fireguiquenbr@gmail.com>
2023-11-20 22:37:21 -0300

    Update Aula_024___array_V.java

--- Cursos/JavaViradoNoJiraya/src/introducao/Aula_024___array_V.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/Aula_024___array_V.java
@@ -1,19 +1,38 @@
+// ARRAY: iterando um array multidimensional mais facilmente
+/*
+    O conceito de "for each" que aprendemos na aula "array_III" também pode se aplicar
+    para arrays multidimensionais.
+*/
 package introducao;
 
 public class Aula_024___array_V
 {
     public static void main(String[] args)
     {
-        int[][] lista = { {50, 20}, {10, 20}};
+        int[][] lista = { {50, 20}, {10, 20} };
 
-
-        for (int arrayElement[] : lista)
+        // cada iteração deste 'for' a variável "mainArr" recebe um array.
+        // como a variável "mainArr" irá receber um array, ela precisa conter os "[]"
+        for (int mainArr[]: lista)
         {
-            for (int num : arrayElement){
+            // cada iteração deste 'for' a variável "num" recebe um elemento do array contido na variável "mainArr"
+            for (int num: mainArr){
                 System.out.println(num);
             }
         }
 
 
+        // EXPLICANDO
+        /*
+            No primeiro ciclo:
+                mainArr[] = lista[0] // {50, 20}
+                        num = mainArr[0]
+                        num = mainArr[1]
+
+            No segundo ciclo:
+                mainArr[] = lista[1] // {10, 20}
+                        num = mainArr[0]
+                        num = mainArr[1]
+        */
     }
 }
