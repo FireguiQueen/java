b2ad9e866721c7f18ea0fce53e597523b28551b7
Guilherme <Fireguiquenbr@gmail.com>
2023-11-23 16:52:38 -0300

    update

---
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/exercicios/carsObjects.java
@@ -0,0 +1,16 @@
+package javacore.A_orientacaoAObjetos.exercicios;
+import javacore.A_orientacaoAObjetos.exercicios.utilitarios.car;
+
+public class carsObjects
+{
+    public static void main(String[] args)
+    {
+        car fiatUno = new car();
+
+        fiatUno.nome = "Fiat uno";
+        fiatUno.modelo = "way";
+        fiatUno.ano = 2013;
+
+        System.out.println("Nome: " + fiatUno.nome + "\nModelo: " + fiatUno.modelo + "\nAno: " + fiatUno.ano);
+    }
+}
---
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/exercicios/utilitarios/car.java
@@ -0,0 +1,9 @@
+package javacore.A_orientacaoAObjetos.exercicios.utilitarios;
+
+public class car
+{
+    public String nome;
+    public String modelo;
+    public short ano;
+
+}
