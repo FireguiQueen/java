afb8f74fcf3dd23e26f19139b23145446e00db85
Guilherme <Fireguiquenbr@gmail.com>
2023-11-23 19:20:37 -0300

    Update main.java

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/variaveisDeReferencia/main.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/variaveisDeReferencia/main.java
@@ -1,4 +1,10 @@
 package javacore.A_orientacaoAObjetos.variaveisDeReferencia;
 
 public class main {
+
+    public int idade;
+
+    public static void main(String[] args) {
+        System.out.println(new main());
+    }
 }
