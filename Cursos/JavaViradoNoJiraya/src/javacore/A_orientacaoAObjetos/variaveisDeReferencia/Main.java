e3242b77248c61ace23f7afc128ee60f0ffb12a4
Guilherme <Fireguiquenbr@gmail.com>
2023-11-23 19:34:49 -0300

    Update Main.java

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/variaveisDeReferencia/Main.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/variaveisDeReferencia/Main.java
@@ -11,11 +11,17 @@ public class Main {
         Estudante aluno = new Estudante();
 
         // Agora, o valor da variável 'aluno' é, na verdade, um endereço de memória daquele objeto.
-        System.out.println(aluno); // Exibe o endereço onde o objeto criado está alocado, por exemplo, '5acf9800'.
+        System.out.println(aluno); // 5acf9800 (endereço onde o objeto criado está alocado)
 
         // Por consequência, se criarmos uma nova variável e atribuirmos a variável 'aluno':
         // Ambas as variáveis irão apontar para o mesmo objeto criado.
         Estudante mesmoAluno = aluno;
         System.out.println(mesmoAluno); // 5acf9800.
+
+
+        // Se a gente atribuir a variável 'aluno' uma nova referência, a refereência antiga será perdida, e nunca mais seremos capazes de acessar o objeto criado.
+        // Por consequência, este objeto perdido tende a ser excluido pela JVM para o programa utilizar menos recursos.
+        aluno = new Estudante(); // agora a variável 'aluno' está armazenando outro endereço.
+        System.out.println(aluno); // 4617c264 
     }
 }
