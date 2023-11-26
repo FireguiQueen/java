38de75e6d12f176814287a77753935f5c0016b86
Guilherme <Fireguiquenbr@gmail.com>
2023-11-26 09:20:12 -0300

    Update trianguloMario.java

--- Cursos/JavaViradoNoJiraya/src/introducao/exercicios/trianguloMario.java
+++ Cursos/JavaViradoNoJiraya/src/introducao/exercicios/trianguloMario.java
@@ -3,12 +3,26 @@ import java.util.Scanner;
 
 public class trianguloMario
 {
+
     public static void main(String[] args)
     {
+        int tamanhoPiramide = takeInfomations();
 
+        for (int i = 0; i < tamanhoPiramide; i++)
+        {
+            System.out.println("oi");
+        }
+
+    }
+
+
+    public static int takeInfomations()
+    {
         Scanner userIn = new Scanner(System.in);
+
         System.out.print("Insira o tamanho da pirâmide: ");
         int tamanhoPiramide;
+
         while (true)
         {
             tamanhoPiramide = userIn.nextInt();
@@ -16,15 +30,8 @@ public class trianguloMario
             if (tamanhoPiramide <= 0) continue;
             break;
         }
-
-        for (int i = 0; i < tamanhoPiramide; i++)
-        {
-            for (int j = 1; j < ; j++) {
-
-            }
-            System.out.println("oi");
-        }
-
-
+        return tamanhoPiramide;
     }
+
+
 }
