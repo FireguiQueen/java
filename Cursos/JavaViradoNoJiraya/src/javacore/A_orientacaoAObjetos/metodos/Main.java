cf3b14dc075f6f06d78f8714fe31232955870ebd
Guilherme <Fireguiquenbr@gmail.com>
2023-11-26 23:08:35 -0300

    Update Main.java

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/metodos/Main.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/metodos/Main.java
@@ -4,13 +4,13 @@ public class Main
 {
     public static void main(String[] args)
     {
-        // Criando um objeto utilizando a classe de domínio 'Aviao'.
+        // Criando um objeto utilizando a classe-de-domínio 'Aviao'.
+        // O objeto é criado e sua referência é armazenada na variável "boeing747".
         Aviao boeing747 = new Aviao();
 
         // Podemos invocar (executar) nossos métodos agora.
         boeing747.abrirPortas();
         boeing747.fecharPortas();
-
 
         // O nosso método "quantidadePoltronas" faz um retorno.
         // Este retorno precisa ser colocado em algum lugar.
@@ -20,9 +20,7 @@ public class Main
         // Mas não somos obrigados a colocá-los numa variável, podemos fazer isto:
         System.out.println(boeing747.quantidadePoltronas());
 
-
         /* ** ** ***** **** ***** ** ** ***** **** ***** ***** *** ** * ************ ***** *** ** * ************ */
-
 
         // Aqui estamos usando uma função que possui parâmetros, sendo assim, ela precisa receber argumentos.
         // Os parâmetros são variáveis locais do método, os argumentos são os valores que aquelas variáveis irão receber.
