619095f4b67785635ba23a197244feab5e4d8fdf
Guilherme <Fireguiquenbr@gmail.com>
2023-11-29 19:03:15 -0300

    Create Main.java

---
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/Varargs/Main.java
@@ -0,0 +1,24 @@
+// VarArgs
+/*
+    Um método também pode receber um array através do parâmetro.
+    E para isso, podemos fazer de duas formas.
+*/
+package javacore.A_orientacaoAObjetos.ClassesEObjetos.Varargs;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    // Método que recebe um array da forma convencional
+    public static void receberLista(int[] lista)
+    {
+        System.out.println(lista);
+    }
+
+    // Método que recebe um array usando o varArgs
+
+
+}
