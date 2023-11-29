0146c3491ef5a2f7598f9f9eb95557aac302da7f
Guilherme <Fireguiquenbr@gmail.com>
2023-11-29 19:18:03 -0300

    Update Main.java

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/Varargs/Main.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/Varargs/Main.java
@@ -9,16 +9,32 @@ public class Main
 {
     public static void main(String[] args)
     {
+        // Este método espera receber um array de inteiros como argumento!
+        int[] numeros = new int[]{50, 10, 49, 39};
+        reciveList(numeros);
 
+        // Este método NÃO espera receber um array como argumentos.
+        // Passe os valores normalmente, e os separe por vírgula. No final, eles estarão dentro de um array.
+        reciveList(5,4,3,2);
     }
 
     // Método que recebe um array da forma convencional
     public static void receberLista(int[] lista)
     {
-        System.out.println(lista);
+        for (int num: lista)
+        {
+            System.out.println(num);
+        }
     }
 
-    // Método que recebe um array usando o varArgs
+    // Este método irá receber diversos argumentos separados por vírgula, e depois, os agrupará criando um array.
+    public static void reciveList(int... lista)
+    {
+        for (int numero: lista)
+        {
+            System.out.println(numero);
+        }
+    }
 
 
 }
