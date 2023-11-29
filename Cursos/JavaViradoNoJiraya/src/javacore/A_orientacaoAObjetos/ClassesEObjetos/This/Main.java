8c6cedc4783a9b249247d87641832e43075022d9
Guilherme <Fireguiquenbr@gmail.com>
2023-11-29 18:55:28 -0300

    This
    Explicando o this em um package separado.

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/This/Main.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/This/Main.java
@@ -1,4 +1,25 @@
-package javacore.A_orientacaoAObjetos.ClassesEObjetos.This;
+// THIS
+/*
+    A palavra-chave 'this' é comumente utilizada em métodos de classe-de-domínio.
 
-public class Main {
+    Quando o this é utilizado em um método por exemplo, ele se refere ao contexto atual. (não entendeu nada, né?)
+    Para entender melhor, você pode acessar a classe "Aviao" no pacote "dominios" para ver a utilização do 'this'.
+*/
+
+package javacore.A_orientacaoAObjetos.ClassesEObjetos.This;
+import javacore.A_orientacaoAObjetos.ClassesEObjetos.This.dominios.Aviao;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        // Criando objetos e salvando o endeço de memória deles nas variáveis.
+        Aviao boeing747 = new Aviao();
+        Aviao boeing737 = new Aviao();
+        Aviao boeing777 = new Aviao();
+
+        boeing747.nome = "Boeing-747";
+        boeing747.ano = 1969;
+        boeing747.mostrarInformacoes();
+    }
 }
