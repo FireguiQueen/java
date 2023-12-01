b36e3d59c630603499fbca44d6a2a2a94ac5adf9
Guilherme <Fireguiquenbr@gmail.com>
2023-11-30 22:04:12 -0300

    Update Aluno.java

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ArquiteturaDeSoftware/Cohesion/Aluno.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ArquiteturaDeSoftware/Cohesion/Aluno.java
@@ -1,15 +1,22 @@
 // COESÃO
 /*
-    Coesão, um princípio fundamental no design orientado a objetos, refere-se à organização e foco de uma classe,
-    garantindo que ela tenha um propósito único e bem definido. Em termos simples, quanto mais focada uma classe,
-    maior será a sua coesão.
+    A coesão é um princípio fundamental no design orientado a objetos; ela se refere à organização e foco de uma classe,
+    garantindo que ela tenha um propósito único e bem definido.
+    Em termos simples, quanto mais específica uma classe, maior será a sua coesão.
 
-    Considere criar duas classes: "Aluno" e "Professor". A classe "Aluno" terá propriedades como "nome", "idade",
-    "id", "anoDeEscolaridade" e etc. A classe "professor" terá "nome", "idade", "id", "disciplina" e etc.
+    Crie classes de propósito único. Isto é, se você deseja criar uma classe responsável por instanciar objetos
+    que representarão estudantes de uma faculdade, a única coisa que esta classe deve conter são as propriedades
+    relacionadas aos alunos.
 
-    Tecnicamente, é totalmente possível criar apenas uma única classe representando tanto o 'aluno'
-    quanto o 'professor', no entanto, essa abordagem NÃO é coesa, pois mistura responsabilidades e
-    torna o código menos claro.
+    ------------------------------------------------------------------
+
+    Considere criar duas classes: "Aluno" e "Professor".
+    A classe "Aluno" terá propriedades como "nome", "idade", "id", "anoDeEscolaridade" e etc.
+    A classe "professor" terá "nome", "idade", "id", "disciplina" e etc.
+
+    É totalmente possível criar apenas uma única classe representando tanto o 'aluno' quanto o 'professor'!
+    No entanto, essa abordagem NÃO é coesa, pois mistura responsabilidades, o correto a se fazer é criar uma
+    classe "Aluno" e uma classe "Professor".
 */
 
 package javacore.A_orientacaoAObjetos.ArquiteturaDeSoftware.Cohesion;
@@ -28,5 +35,7 @@ public class Aluno
 
 }
 
-// NO PACOTE "DOMINIOS", você pode encontrar dois subpacotes: "coesao" e "nao_coesao". Sinta-se livre para
-// observar com mais clareza este conceito.
+// ** *** ** ** *** *** ** *** ** ** *** ***  ** *** ** ** *** ***
+// NO PACOTE "DOMINIOS", você pode encontrar dois subpacotes: "coesao" e "nao_coesao".
+// Sinta-se livre para observar com mais clareza este conceito.
+// ** *** ** ** *** *** ** *** ** ** *** ***  ** *** ** ** *** ***
