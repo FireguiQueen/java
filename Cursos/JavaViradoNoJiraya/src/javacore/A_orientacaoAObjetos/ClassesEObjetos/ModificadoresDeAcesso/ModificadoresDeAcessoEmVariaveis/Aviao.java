39a28354aea071078cf507f8e66e5630ea9fea15
Guilherme <Fireguiquenbr@gmail.com>
2023-11-30 23:38:14 -0300

    Update Aviao.java

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/ModificadoresDeAcesso/ModificadoresDeAcessoEmVariaveis/Aviao.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/ModificadoresDeAcesso/ModificadoresDeAcessoEmVariaveis/Aviao.java
@@ -1,6 +1,18 @@
-// MODIFICADORES DE ACESSO EM VARIÁVEIS (propriedades) 
+// MODIFICADORES DE ACESSO EM VARIÁVEIS (propriedades)
 /*
+    Talvez você se pergunte:
+    Mas quando criarmos um objeto a apartir desta classe, como vamos definir e retornar os valores dessas propriedades?
+    Afinal, elas estão privadas, ou seja, nenhuma outra classe terá acesso a elas.
 
+    Bem isto é verdade, nenhuma classe terá acesso as variáveis privadas de objetos criados a partir desta classe.
+    E para isso, iremos definir métodos públicos.
+    Eles são: get e set.
+
+    O método 'set' tem o objetivo de definir um valor para a propriedade do objeto.
+    Para isso, criaremos um parâmetro para ele, e quando o objeto for invocado em outra classe juntamente a este
+    metodo, ele irá passar como argumento o valor que deseja atribruir a propriedade.
+
+    O método 'get' tem o objetivo de pegar um valor de uma propriedade e retornar ele. 
 */
 package javacore.A_orientacaoAObjetos.ClassesEObjetos.ModificadoresDeAcesso.ModificadoresDeAcessoEmVariaveis;
 
