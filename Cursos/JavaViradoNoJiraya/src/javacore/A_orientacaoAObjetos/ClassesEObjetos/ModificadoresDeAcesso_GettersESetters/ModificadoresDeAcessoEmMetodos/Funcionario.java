f175ccdec85813b16f952ce278fa6d67c0d6d35f
Guilherme <Fireguiquenbr@gmail.com>
2023-11-30 23:19:45 -0300

    update

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/ModificadoresDeAcesso_GettersESetters/ModificadoresDeAcessoEmMetodos/Funcionario.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/ModificadoresDeAcesso_GettersESetters/ModificadoresDeAcessoEmMetodos/Funcionario.java
@@ -1,5 +1,25 @@
+// MODIFICADORES DE ACESSO EM MÉTODOS
+/*
+    Quando definimos que um método é "private", significa que apenas as instruções do objeto terão acesso
+    a ele.
+
+*/
 package javacore.A_orientacaoAObjetos.ClassesEObjetos.ModificadoresDeAcesso_GettersESetters.ModificadoresDeAcessoEmMetodos;
 
 public class Funcionario
 {
+    public void mensagemBoasVindas()
+    {
+        System.out.println("Olá, seja muito bem vindo!");
+    }
+
+    private void historiaEmpresa()
+    {
+        System.out.println("Somos a &-Company, e contratamos apenas os mais qualificados do mercado. Nossa empresa surgiu em meados de 70, com o objetivo de.. .. ");
+    }
+
+    public void exibirHistoriaEmpresa()
+    {
+        historiaEmpresa();
+    }
 }
