02ddb51d445c519e6c092920c1b785f7c58c4f36
Guilherme <Fireguiquenbr@gmail.com>
2023-12-04 21:26:09 -0300

    Melhora de explicação
    Arrumei o texto sobre o modificador de acesso sendo utilizado em classes de dominio

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/ModificadoresDeAcesso/ModificadoresDeAcessoEmClasses/Estudante.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/ModificadoresDeAcesso/ModificadoresDeAcessoEmClasses/Estudante.java
@@ -1,23 +1,29 @@
 // MODIFICADORES DE ACESSO:
 /*
     Podemos ver que sempre quando definimos uma classe, um método, ou até mesmo uma variável, nós utilizamos
-    um dos modificadores de acesso, o "public".
+    um dos modificadores de acesso: o "public".
 
-    Quando definimos uma classe pública, como no exemplo abaixo, estamos dizendo que outras classes do nosso programa
-    podem acessar ela.
-    Esta nossa classe ("estudante") é uma classe-de-domínio, isto é, ela é uma classe que será utilizada para instanciar
+    Quando definimos uma classe pública, estamos dizendo que outras classes do nosso programa
+    podem fazer uso dela, ou seja, criar objetos.
+
+    /// /// /// ////// ////// ////// ////// ////// ////// ////// ////// ////// ///
+
+    A classe 'Estudante' é uma classe-de-domínio, isto é, ela é uma classe que será utilizada para instanciar
     objetos que representarão estudantes.
-    E para isso, ela PRECISA ser pública, pois se ela fosse "private" (privada) não teríamos acesso a ela em outras
-    classes.
+    Para isso, ela precisa ser pública, pois se ela fosse "private" (privada) não teríamos acesso a ela em outras
+    classes, consequentemencia, seria impossível criar objetos atraves desta classe.
 
-    Veja bem, quando você for utilizar esta classe na sua classe principal, você dará a seguinte instrução:
+    /// /// /// ////// ////// ////// ////// ////// ////// ////// ////// ////// ///
+
+    Quando você for utilizar esta classe na sua classe 'Main', você dará a seguinte instrução:
     > Estudante aluno001 = new Estudante();
 
-    E fazer isto só é possível pois nossa classe está publica.
+    E fazer isto só é possível pois nossa classe está pública.
 */
 
 package javacore.A_orientacaoAObjetos.ClassesEObjetos.ModificadoresDeAcesso.ModificadoresDeAcessoEmClasses;
 
 public class Estudante
 {
+    // ... 
 }
--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/ModificadoresDeAcesso/ModificadoresDeAcessoEmVariaveis/Main.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ClassesEObjetos/ModificadoresDeAcesso/ModificadoresDeAcessoEmVariaveis/Main.java
@@ -1,4 +1,9 @@
 package javacore.A_orientacaoAObjetos.ClassesEObjetos.ModificadoresDeAcesso.ModificadoresDeAcessoEmVariaveis;
 
-public class Main {
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
 }
