3265383de764c7d15620ca9d53cb1684c63fca76
Guilherme <Fireguiquenbr@gmail.com>
2023-12-05 21:33:02 -0300

    update

---
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/Constructor/Main.java
@@ -0,0 +1,12 @@
+package javacore.A_orientacaoAObjetos.Constructor;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+        // Construtor: 'Estudante(..)'
+        // Precisamos obrigatoriamente passar argumentos, pois este construtor possui parâmetros.
+        Estudante aluno001 = new Estudante("Flavio", 14);
+    }
+}
