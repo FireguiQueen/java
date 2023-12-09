96a9996b6d6dfe3cc68bef6456a743efd03125a7
Guilherme <Fireguiquenbr@gmail.com>
2023-12-09 19:58:48 -0300

    Update Anime.java

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/BlocosDeInicializacao/Anime.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/BlocosDeInicializacao/Anime.java
@@ -2,11 +2,11 @@
 /*
     Blocos de inicialização são blocos de alta prioridade.
 
-    Podemos pensar na ordem desta forma:
-    1. Alocar espaço em memória para este objeto
-    2. As propriedades são iniciadas com valores padrão ou valores definidos da classe
-    3. O bloco de inicialização é executado (caso haja um)
-    4. O constructor é executado
+    Ao criarmos um objeto, podemos pensar na seguinte ordem:
+    1. É alocado espaço em memória para este objeto;
+    2. As propriedades são inicializadas com valores default (0, null..) ou valores definidos;
+    3. O bloco de inicialização é executado (caso haja um);
+    4. O constructor é executado. Podendo reescrever os valores das propriedades.
 */
 
 package javacore.A_orientacaoAObjetos.BlocosDeInicializacao;
@@ -20,11 +20,12 @@ public class Anime
         this.nome = nome;
     }
 
-    // Isto é um bloco de inicialização.
+    // bloco de inicialização:
     {
         /*
-            Isto sempre retornará "null". Pois como o bloco de inicialização é sempre executado antes do
-            constructor, a propriedade "nome" ainda não possui um valor, apenas o valor definido por padrão (null)
+            Sempre que um objeto for criado, será printado "NULL".
+            -> O bloco de inicialização é sempre executado antes do constructor.
+            -> A propriedade "nome" ainda não possui um valor, apenas o valor definido por default (NULL)
         */
         System.out.println(this.nome);
     }
