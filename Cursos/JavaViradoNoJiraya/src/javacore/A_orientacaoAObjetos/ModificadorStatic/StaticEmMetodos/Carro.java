38127559089eb8164cedc63e8bd9a5b2ec8582a0
Guilherme <Fireguiquenbr@gmail.com>
2023-12-11 20:01:02 -0300

    update

--- Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ModificadorStatic/StaticEmMetodos/Carro.java
+++ Cursos/JavaViradoNoJiraya/src/javacore/A_orientacaoAObjetos/ModificadorStatic/StaticEmMetodos/Carro.java
@@ -3,9 +3,9 @@
     Quando os métodos são declarados como "static", estamos dizendo que este método pertence a classe,
     e não aos objetos instânciados.
 
-    Um método estático não pode utilizar a palavra "this".
-    Eis o motivo:
+    ** MÉTODOS ESTÁTICOS NÃO PODEM UTILIZAR A PALAVRA-CHAVE "THIS" **
 
+    Eis o motivo:
         -> Quando invocamos um método, o 'this' dentro deste método fará referência ao objeto em que
         executou o método.
 
@@ -20,7 +20,7 @@
             }
 
 
-        -> Porém, quando há um método "static", não há necessidade de criar um objeto para invocá-lo, afinal
+        -> Porém, quando há     um método "static", não há necessidade de criar um objeto para invocá-lo, afinal
         o método pertence a classe, e por isso, podemos chamá-lo pela própria classe:
             >> Carro.getInf();
 
@@ -35,15 +35,41 @@
         Exatamente, nenhum.
         Por isso não podemos utilizar a palavra "this" em métodos não-estáticos.
 
-        
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+        ** MÉTODOS ESTÁTICOS NÃO PODEM CHAMAR VARIÁVEIS NÃO-ESTÁTICAS **
+
+        Eis o motivo:
+            Em uma classe de domínio, variáveis não-estáticas são variáveis de instância.
+
+            Um método estático (um método que pertence a classe) não pode acessar uma variável
+            de instância, pois ela só irá existir em objetos instânciados pela classe.
+
 */
 package javacore.A_orientacaoAObjetos.ModificadorStatic.StaticEmMetodos;
 
 public class Carro
 {
+    // DEFININDO GET E SETTERS PARA VARIÁVEIS ESTÁTICAS PRIVADAS
+    /*
+        Posso criar um método get/set não-estático para pegar/definir o valor de uma variável estática?
+        --> Sim, você pode, mas você não deveria.
 
-    public static void setCarro()
+        Se sua variável é estática, significa que ela pertence a classe.
+        Logo, os métodos getters e seterrs deveriam ser acessíveis diretamente pela classe, e não por um objeto.
+    */
+    private static int numeroFixoDeRodaParaCarros = 4;
+
+    // Boa prática
+    public static int getNumeroFixoDeRodaParaCarros()
     {
-        this.
+        return Carro.numeroFixoDeRodaParaCarros;
+    }
+
+    // Má prática
+    public int getNumeroFixoDeRodaParaCarross()
+    {
+        return Carro.numeroFixoDeRodaParaCarros;
     }
 }
