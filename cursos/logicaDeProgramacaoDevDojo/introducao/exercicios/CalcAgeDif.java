047c7e95ea8fe24c61401e9c1cded99691af4cca
Guilherme <Fireguiquenbr@gmail.com>
2024-05-08 22:48:28 -0300

    Update CalcAgeDif.java

--- cursos/logicaDeProgramacaoDevDojo/introducao/exercicios/CalcAgeDif.java
+++ cursos/logicaDeProgramacaoDevDojo/introducao/exercicios/CalcAgeDif.java
@@ -3,9 +3,8 @@ public class CalcAgeDif
     public static void main(String[] args)
     {
         int age_01 = 40;
-        int age_02 = 55;
+        int age_02 = 50;
         int difAge = age_01 - age_02;
-
-        System.out.println(difAge);
+        System.out.println("Idade: " + difAge);
     }
 }
