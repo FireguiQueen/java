afba10fc4c6dac45fa40ef934db7aa1cd0d9e1cc
Guilherme <Fireguiquenbr@gmail.com>
2024-05-08 22:40:10 -0300

    Create CalcAgeDif.java

---
+++ cursos/logicaDeProgramacaoDevDojo/introducao/exercicios/CalcAgeDif.java
@@ -0,0 +1,11 @@
+public class CalcAgeDif
+{
+    public static void main(String[] args)
+    {
+        int age_01 = 40;
+        int age_02 = 55;
+        int difAge = age_01 - age_02;
+
+        System.out.println(difAge);
+    }
+}
