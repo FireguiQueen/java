161f6ae1c0aa2dffa1c1619ba7a0e8308d9c1968
Guilherme <Fireguiquenbr@gmail.com>
2024-05-09 21:05:27 -0300

    update

---
+++ cursos/logicaDeProgramacaoDevDojo/introducao/exercicios/ReutilizandoValores.java
@@ -0,0 +1,18 @@
+// %: 30, 15 and 5.
+
+public class ReutilizandoValores
+{
+    public static void main(String[] args)
+    {
+        double valorInicial = 900;
+
+        double resultado = valorInicial * 0.30;
+        System.out.println("30% de " + valorInicial + " é: " + resultado);
+
+        resultado = valorInicial * 0.15;
+        System.out.println("15% de " + valorInicial + " é: " + resultado);
+
+        resultado = valorInicial * 0.05;
+        System.out.println("5% de " + valorInicial + " é: " + resultado);
+    }
+}
