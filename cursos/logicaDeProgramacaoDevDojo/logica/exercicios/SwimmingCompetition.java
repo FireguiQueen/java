4b810ad612d29959e4b243162e65a88c37692615
Guilherme <98475125+FireguiQueen@users.noreply.github.com>
2024-05-18 22:10:07 -0300

    Update SwimmingCompetition.java

--- cursos/logicaDeProgramacaoDevDojo/logica/exercicios/SwimmingCompetition.java
+++ cursos/logicaDeProgramacaoDevDojo/logica/exercicios/SwimmingCompetition.java
@@ -1,7 +1,29 @@
-public class SwimmingCompetiton 
+public class SwimmingCompetition
 {
     public static void main(String[] args)
-  {
-    
-  }
+    {
+        String name = "Otavio";
+        String categoria = "";
+        short age = 22;
+
+        if (age < 10)
+        {
+            categoria = "infantil";
+        } 
+        else if (age >= 11 && age <= 15)
+        {
+            categoria = "juvenil";   
+        }
+        else if (age >= 16 && age <= 19)
+        {
+            categoria = "pre-adulto";   
+        } else 
+        {
+            categoria = "adulto";   
+        }
+
+        System.out.print(name + ": participará da categoria " + categoria);   
+        
+        
+    }
 }
