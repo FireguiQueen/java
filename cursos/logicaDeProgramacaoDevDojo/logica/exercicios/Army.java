f81b04797d5a9f058c3b34a37db07c086e5e8f51
Guilherme <Fireguiquenbr@gmail.com>
2024-05-20 19:14:07 -0300

    Create Army.java

---
+++ cursos/logicaDeProgramacaoDevDojo/logica/exercicios/Army.java
@@ -0,0 +1,22 @@
+public class Army
+{
+    public static void main(String[] args)
+    {
+        short age = 14;
+        char genero = 'F';
+        boolean alistamentoObrigatorio = false;
+
+        if (age < 18)
+        {
+            System.out.println("Não pode se alistar.");
+        } else if (age >= 18 && genero == 'F')
+        {
+            alistamentoObrigatorio = true;
+        } else {
+            alistamentoObrigatorio = false;
+        }
+
+        System.out.print("Alistamento: ");
+        System.out.println(alistamentoObrigatorio? "obrigatório" : "não-obrigatório");
+    }
+}
