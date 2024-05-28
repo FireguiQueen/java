34bcede28b133e6e0c0892b841d43593bd93aab9
Guilherme <Fireguiquenbr@gmail.com>
2024-05-27 21:04:06 -0300

    update

---
+++ cursos/logicaDeProgramacaoDevDojo/logica/exercicios/MultiplicationTable.java
@@ -0,0 +1,9 @@
+public class MultiplicationTable
+{
+    public static void main(String[] args) {
+        int setNumber = 9;
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("" + setNumber + " * " + i + " = " + (setNumber * i));
+        }
+    }
+}
---
+++ cursos/logicaDeProgramacaoDevDojo/logica/exercicios/QuadradoPerfeito.java
@@ -0,0 +1,10 @@
+public class QuadradoPerfeito
+{
+    public static void main(String[] args) {
+        for (int i = 1; i <= 200 ; i++)
+        {
+            System.out.println("Quadrado perfeito: " + (i*i) + " (" + i +  " * " + i + ")");
+
+        }
+    }
+}
