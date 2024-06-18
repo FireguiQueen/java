a5fbc5078f064f26085168ff47c0c9b41625359a
Guilherme <fireguiquenbr@gmail.com>
2024-06-18 13:47:09 -0300

    final commit

--- cursos/logicaDeProgramacaoDevDojo/src/logica/00.exercicios/MultiplicationTable.java
+++
@@ -1,9 +0,0 @@
-public class MultiplicationTable
-{
-    public static void main(String[] args) {
-        int setNumber = 9;
-        for (int i = 0; i <= 10; i++) {
-            System.out.println("" + setNumber + " * " + i + " = " + (setNumber * i));
-        }
-    }
-}
